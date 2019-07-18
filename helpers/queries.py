6206ee1cec8bf29fb0927494fc527b1cf9848aed
Edouard d'Archimbaud <edouard@kili-technology.com>
2019-07-18 11:53:29 +0200

    Update to new version nof the API

---
+++ helpers/queries.py
@@ -0,0 +1,105 @@
+from json import loads
+
+def get_projects(client, user_id):
+    result = client.execute('''
+    query {
+      getProjects(userID: "%s") {
+        id
+        title
+        description
+        author {
+            id
+            email
+        }
+        createdAt
+        interfaceTools {
+            name
+            toolType
+            jsonSettings
+        }
+        roles {
+            user {
+                id
+                email
+            }
+            role
+            lastLabelingAt
+            numberOfAnnotations
+            totalDuration
+            durationPerLabel
+            honeypotMark
+        }
+        updatedAt
+        useHoneyPot
+        numberOfAssets
+        completionPercentage
+        numberOfAssetsWithoutLabel
+        numberOfAssetsWithEmptyLabels
+        numberOfReviewedAssets
+        numberOfLatestLabels
+        dataset {
+          id
+          honeypotMark
+        }
+      }
+    }
+    ''' % (user_id))
+    return loads(result)['data']['getProjects']
+
+
+def get_assets_by_external_id(client, project_id, external_id):
+    result = client.execute('''
+    query {
+      getAssetsByExternalId(projectID: "%s", externalID: "%s") {
+          id
+          content
+          externalId
+          createdAt
+          updatedAt
+          isHoneypot
+          status
+          labels {
+            author {
+              id
+              email
+            }
+            labelType
+            jsonResponse
+            createdAt
+            millisecondsToLabel
+            totalMillisecondsToLabel
+            honeypotMark
+          }
+      }
+    }
+    ''' % (project_id, external_id))
+    return loads(result)['data']['getAssetsByExternalId']
+
+
+def export_assets(client, project_id):
+    result = client.execute('''
+    query {
+      exportAssets(projectID: "%s") {
+        id
+        content
+        externalId
+        createdAt
+        updatedAt
+        isHoneypot
+        status
+        labels {
+          author {
+            id
+            email
+          }
+          labelType
+          jsonResponse
+          createdAt
+          millisecondsToLabel
+          totalMillisecondsToLabel
+          honeypotMark
+        }
+      }
+    }
+    ''' % (project_id))
+    return loads(result)['data']['exportAssets']
