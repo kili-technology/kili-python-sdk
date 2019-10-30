924af10d8b0c2a4c2a2eaf6f4006fc49ea6f7d30
Edouard d'Archimbaud <edouard@kili-technology.com>
2019-10-30 16:34:30 +0100

    Implementation of AAU, when I create project, I want to use kili-playground

--- kili/authentication.py
+++ kili/authentication.py
@@ -1,14 +1,23 @@
 import os
+import requests
 
-from graphqlclient import GraphQLClient
+from .graphql_client import GraphQLClient
 
 from .mutations.user import signin
+
+MAX_RETRIES = 20
 
 
 def authenticate(email,
                  password=os.getenv('KILI_USER_PASSWORD'),
                  api_endpoint='https://cloud.kili-technology.com/api/label/graphql'):
-    client = GraphQLClient(api_endpoint)
+
+    session = requests.Session()
+    adapter = requests.adapters.HTTPAdapter(max_retries=MAX_RETRIES)
+    session.mount('https://', adapter)
+    session.mount('http://', adapter)
+    client = GraphQLClient(api_endpoint, session)
+
     auth_payload = signin(client, email, password)
     api_token = auth_payload['token']
     client.inject_token('Bearer: ' + api_token)
