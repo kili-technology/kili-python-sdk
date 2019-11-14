74b97e6ee2f3eef018c5fc829c527152f36887b7
Edouard d'Archimbaud <edouard@kili-technology.com>
2019-11-14 18:41:57 +0100

    Implementation of authentication session

---
+++ kili/playground.py
@@ -0,0 +1,221 @@
+import kili.mutations.asset
+import kili.mutations.label
+import kili.mutations.lock
+import kili.mutations.organization
+import kili.mutations.project
+import kili.mutations.tool
+import kili.mutations.user
+import kili.quality.consensus
+import kili.quality.honeypot
+import kili.queries.asset
+import kili.queries.label
+import kili.queries.lock
+import kili.queries.project
+import kili.queries.tool
+import kili.queries.user
+
+
+class Playground(object):
+
+    def __init__(self, auth=None):
+        """Create an instance of KiliPlayground."""
+        self.auth = auth
+
+    # Mutations Asset
+
+    def create_assets(self, **kwargs):
+        return kili.mutations.asset.create_assets(self.auth.client, **kwargs)
+
+    def delete_assets_by_external_id(self, **kwargs):
+        return kili.mutations.asset.delete_assets_by_external_id(self.auth.client, **kwargs)
+
+    def append_to_dataset(self, **kwargs):
+        return kili.mutations.asset.append_to_dataset(self.auth.client, **kwargs)
+
+    def append_many_to_dataset(self, **kwargs):
+        return kili.mutations.asset.append_many_to_dataset(self.auth.client, **kwargs)
+
+    def update_asset(self, **kwargs):
+        return kili.mutations.asset.update_asset(self.auth.client, **kwargs)
+
+    def update_properties_in_asset(self, **kwargs):
+        return kili.mutations.asset.update_properties_in_asset(self.auth.client, **kwargs)
+
+    def delete_from_dataset(self, **kwargs):
+        return kili.mutations.asset.delete_from_dataset(self.auth.client, **kwargs)
+
+    def delete_many_from_dataset(self, **kwargs):
+        return kili.mutations.asset.delete_many_from_dataset(self.auth.client, **kwargs)
+
+    def force_update_status(self, **kwargs):
+        return kili.mutations.asset.force_update_status(self.auth.client, **kwargs)
+
+    # Mutations Label
+
+    def create_prediction(self, **kwargs):
+        return kili.mutations.label.create_prediction(self.auth.client, **kwargs)
+
+    def append_to_labels(self, **kwargs):
+        return kili.mutations.label.append_to_labels(self.auth.client, **kwargs)
+
+    def update_label(self, **kwargs):
+        return kili.mutations.label.update_label(self.auth.client, **kwargs)
+
+    def update_properties_in_label(self, **kwargs):
+        return kili.mutations.label.update_properties_in_label(self.auth.client, **kwargs)
+
+    # Mutations Lock
+
+    def delete_locks(self, **kwargs):
+        return kili.mutations.lock.delete_locks(self.auth.client, **kwargs)
+
+    # Mutations Organization
+
+    def create_organization(self, **kwargs):
+        return kili.mutations.organization.create_organization(self.auth.client, **kwargs)
+
+    def update_organization(self, **kwargs):
+        return kili.mutations.organization.update_organization(self.auth.client, **kwargs)
+
+    def delete_organization(self, **kwargs):
+        return kili.mutations.organization.delete_organization(self.auth.client, **kwargs)
+
+    # Mutations Project
+
+    def create_project(self, **kwargs):
+        return kili.mutations.project.create_project(self.auth.client, **kwargs)
+
+    def delete_project(self, **kwargs):
+        return kili.mutations.project.delete_project(self.auth.client, **kwargs)
+
+    def append_to_roles(self, **kwargs):
+        return kili.mutations.project.append_to_roles(self.auth.client, **kwargs)
+
+    def update_properties_in_project(self, **kwargs):
+        return kili.mutations.project.update_properties_in_project(self.auth.client, **kwargs)
+
+    def update_interface_in_project(self, **kwargs):
+        return kili.mutations.project.update_interface_in_project(self.auth.client, **kwargs)
+
+    def create_empty_project(self, **kwargs):
+        return kili.mutations.project.create_empty_project(self.auth.client, **kwargs)
+
+    def update_project(self, **kwargs):
+        return kili.mutations.project.update_project(self.auth.client, **kwargs)
+
+    def update_role(self, **kwargs):
+        return kili.mutations.project.update_role(self.auth.client, **kwargs)
+
+    def delete_from_roles(self, **kwargs):
+        return kili.mutations.project.delete_from_roles(self.auth.client, **kwargs)
+
+    def update_properties_in_project_user(self, **kwargs):
+        return kili.mutations.project.update_properties_in_project_user(self.auth.client, **kwargs)
+
+    def force_project_kpis(self, **kwargs):
+        return kili.mutations.project.force_project_kpis(self.auth.client, **kwargs)
+
+    # Mutations Tool
+
+    def update_tool(self, **kwargs):
+        return kili.mutations.tool.update_tool(self.auth.client, **kwargs)
+
+    def append_to_tools(self, **kwargs):
+        return kili.mutations.tool.append_to_tools(self.auth.client, **kwargs)
+
+    def delete_from_tools(self, **kwargs):
+        return kili.mutations.tool.delete_from_tools(self.auth.client, **kwargs)
+
+    # Mutations User
+
+    def create_user(self, **kwargs):
+        return kili.mutations.user.create_user(self.auth.client, **kwargs)
+
+    def create_user_from_email_if_not_exists(self, **kwargs):
+        return kili.mutations.user.create_user_from_email_if_not_exists(self.auth.client, **kwargs)
+
+    def update_user(self, **kwargs):
+        return kili.mutations.user.update_user(self.auth.client, **kwargs)
+
+    def update_password(self, **kwargs):
+        return kili.mutations.user.update_password(self.auth.client, **kwargs)
+
+    def reset_password(self, **kwargs):
+        return kili.mutations.user.reset_password(self.auth.client, **kwargs)
+
+    # Quality Consensus
+
+    def update_consensus_in_many_assets(self, **kwargs):
+        return kili.quality.consensus.update_consensus_in_many_assets(self.auth.client, **kwargs)
+
+    def force_consensus_for_project(self, **kwargs):
+        return kili.quality.consensus.force_consensus_for_project(self.auth.client, **kwargs)
+
+    # Quality Honeypot
+
+    def create_honeypot(self, **kwargs):
+        return kili.quality.honeypot.create_honeypot(self.auth.client, **kwargs)
+
+    # Queries Asset
+
+    def get_asset(self, **kwargs):
+        return kili.queries.asset.get_asset(self.auth.client, **kwargs)
+
+    def get_assets(self, **kwargs):
+        return kili.queries.asset.get_assets(self.auth.client, **kwargs)
+
+    def get_assets_by_external_id(self, **kwargs):
+        return kili.queries.asset.get_assets_by_external_id(self.auth.client, **kwargs)
+
+    def get_next_asset_from_label(self, **kwargs):
+        return kili.queries.asset.get_next_asset_from_label(self.auth.client, **kwargs)
+
+    def get_next_asset_from_project(self, **kwargs):
+        return kili.queries.asset.get_next_asset_from_project(self.auth.client, **kwargs)
+
+    def export_assets(self, **kwargs):
+        return kili.queries.asset.export_assets(self.auth.client, **kwargs)
+
+    # Queries Label
+
+    def get_label(self, **kwargs):
+        return kili.queries.label.get_label(self.auth.client, **kwargs)
+
+    def get_latest_labels_for_user(self, **kwargs):
+        return kili.queries.label.get_latest_labels_for_user(self.auth.client, **kwargs)
+
+    def get_latest_labels(self, **kwargs):
+        return kili.queries.label.get_latest_labels(self.auth.client, **kwargs)
+
+    # Queries Lock
+
+    def get_locks(self, **kwargs):
+        return kili.queries.lock.get_locks(self.auth.client, **kwargs)
+
+    # Queries Project
+
+    def get_projects(self, **kwargs):
+        return kili.queries.project.get_projects(self.auth.client, **kwargs)
+
+    def get_project(self, **kwargs):
+        return kili.queries.project.get_project(self.auth.client, **kwargs)
+
+    # Queries Tool
+
+    def get_tools(self, **kwargs):
+        return kili.queries.tool.get_tools(self.auth.client, **kwargs)
+
+    # Queries User
+
+    def get_user(self, **kwargs):
+        return kili.queries.user.get_user(self.auth.client, **kwargs)
+
+
+if __name__ == '__main__':
+    """ Example of usage """
+    from kili.authentication import KiliAuth
+    from kili.playground import Playground
+    kauth = KiliAuth()
+    playground = Playground(kauth)
+    assets = playground.export_assets(project_id="first-project")
+    print(assets)
--- recipes/append_assets_and_predictions_with_automl.py
+++ recipes/append_assets_and_predictions_with_automl.py
@@ -21,10 +21,8 @@ from sklearn.metrics import precision_recall_fscore_support
 import autosklearn
 import autosklearn.classification
 
-from kili.authentication import authenticate
-from kili.mutations.asset import append_to_dataset
-from kili.mutations.label import create_prediction, append_to_labels
-from kili.queries.asset import get_assets_by_external_id, export_assets
+from kili.authentication import KiliAuth
+from kili.playground import Playground
 
 
 NGRAM_RANGE = (1, 2)
@@ -33,6 +31,7 @@ TOKEN_MODE = 'word'
 MIN_DOC_FREQ = 2
 converter_label_to_name = {0: "NEGATIVE", 1: "POSITIVE"}
 converter_name_to_label = {"NEGATIVE": 0, "POSITIVE": 1}
+
 
 def download_dataset():
     url = 'https://ai.stanford.edu/~amaas/data/sentiment/aclImdb_v1.tar.gz'
@@ -43,6 +42,7 @@ def download_dataset():
         urllib.request.urlretrieve(url, target_path)
     return target_path
 
+
 def extract_dataset(path):
     target_path = './IMDB'
     if not os.path.exists(target_path):
@@ -51,11 +51,13 @@ def extract_dataset(path):
         tar.close()
     return target_path
 
+
 def shuffle(X, y):
     perm = np.random.RandomState(seed=10).permutation(len(X))
     X = X[perm]
     y = y[perm]
     return X, y
+
 
 def load_imdb_dataset(path):
     imdb_path = os.path.join(path)
@@ -71,11 +73,15 @@ def load_imdb_dataset(path):
             for fname in sorted(os.listdir(dset_path)):
                 if fname.endswith('.txt'):
                     with open(os.path.join(dset_path, fname)) as f:
-                        if dset == 'train': train_texts.append(f.read())
-                        else: test_texts.append(f.read())
+                        if dset == 'train':
+                            train_texts.append(f.read())
+                        else:
+                            test_texts.append(f.read())
                     label = 0 if cat == 'neg' else 1
-                    if dset == 'train': train_labels.append(label)
-                    else: test_labels.append(label)
+                    if dset == 'train':
+                        train_labels.append(label)
+                    else:
+                        test_labels.append(label)
 
     # Converting to np.array
     train_texts = np.array(train_texts)
@@ -87,9 +93,9 @@ def load_imdb_dataset(path):
     train_texts, train_labels = shuffle(train_texts, train_labels)
     test_texts, test_labels = shuffle(test_texts, test_labels)
 
-
     # Return the dataset
     return train_texts, train_labels, test_texts, test_labels
+
 
 def plot_sample_length_distribution(figure_path, X_train):
     plt.figure(figsize=(15, 10))
@@ -99,13 +105,14 @@ def plot_sample_length_distribution(figure_path, X_train):
     plt.title('Sample length distribution')
     plt.savefig(os.path.join(figure_path, "length_distribution.png"), dpi=200)
 
-def plot_most_frequent_words_or_ngrams(figure_path, X_train, ngram_range = (1,1)):
+
+def plot_most_frequent_words_or_ngrams(figure_path, X_train, ngram_range=(1, 1)):
     kwargs = {
-        'ngram_range' : ngram_range,
-        'dtype' : 'int32',
-        'strip_accents' : 'unicode',
-        'decode_error' : 'replace',
-        'analyzer' : TOKEN_MODE,
+        'ngram_range': ngram_range,
+        'dtype': 'int32',
+        'strip_accents': 'unicode',
+        'decode_error': 'replace',
+        'analyzer': TOKEN_MODE,
     }
     vectorizer = CountVectorizer(**kwargs)
     vect_texts = vectorizer.fit_transform(list(X_train))
@@ -113,7 +120,8 @@ def plot_most_frequent_words_or_ngrams(figure_path, X_train, ngram_range = (1,1)
     num_ngrams = min(50, len(all_ngrams))
     all_counts = vect_texts.sum(axis=0).tolist()[0]
 
-    all_ngrams, all_counts = zip(*[(n, c) for c, n in sorted(zip(all_counts, all_ngrams), reverse=True)])
+    all_ngrams, all_counts = zip(
+        *[(n, c) for c, n in sorted(zip(all_counts, all_ngrams), reverse=True)])
     ngrams = all_ngrams[:num_ngrams]
     counts = all_counts[:num_ngrams]
 
@@ -127,21 +135,22 @@ def plot_most_frequent_words_or_ngrams(figure_path, X_train, ngram_range = (1,1)
     plt.xticks(idx, ngrams, rotation=45)
     plt.savefig(os.path.join(figure_path, "most_frequent_ngrams.png"), dpi=200)
 
+
 def ngram_vectorize(train_texts, train_labels, val_texts):
     kwargs = {
-        'ngram_range' : NGRAM_RANGE,
-        'dtype' : 'int32',
-        'strip_accents' : 'unicode',
-        'decode_error' : 'replace',
-        'analyzer' : TOKEN_MODE,
-        'min_df' : MIN_DOC_FREQ,
+        'ngram_range': NGRAM_RANGE,
+        'dtype': 'int32',
+        'strip_accents': 'unicode',
+        'decode_error': 'replace',
+        'analyzer': TOKEN_MODE,
+        'min_df': MIN_DOC_FREQ,
     }
-    
+
     # Learn Vocab from train texts and vectorize train and val sets
     tfidf_vectorizer = TfidfVectorizer(**kwargs)
     x_train = tfidf_vectorizer.fit_transform(train_texts)
     x_val = tfidf_vectorizer.transform(val_texts)
-    
+
     # Select best k features, with feature importance measured by f_classif
     selector = SelectKBest(f_classif, k=min(TOP_K, x_train.shape[1]))
     selector.fit(x_train, train_labels)
@@ -149,14 +158,19 @@ def ngram_vectorize(train_texts, train_labels, val_texts):
     x_val = selector.transform(x_val).astype('float32')
     return x_train, x_val
 
-def print_infos_dataset(X_train,y_train,X_test,y_test):
-    print ('Train samples shape: {} | train labels shape: {}'.format(X_train.shape, y_train.shape))
-    print ('Test samples shape: {} | test labels shape: {}'.format(X_test.shape, y_test.shape))
-    
+
+def print_infos_dataset(X_train, y_train, X_test, y_test):
+    print('Train samples shape: {} | train labels shape: {}'.format(
+        X_train.shape, y_train.shape))
+    print('Test samples shape: {} | test labels shape: {}'.format(
+        X_test.shape, y_test.shape))
+
     uniq_class_arr, counts = np.unique(y_train, return_counts=True)
-    print ('Number of unique classes : {}'.format(len(uniq_class_arr)))
+    print('Number of unique classes : {}'.format(len(uniq_class_arr)))
     for _class in uniq_class_arr:
-        print ('Counts for class ', uniq_class_arr[_class], ': ', counts[_class])
+        print('Counts for class ',
+              uniq_class_arr[_class], ': ', counts[_class])
+
 
 def extract_train_for_autoML(assets):
     X_train = []
@@ -166,45 +180,48 @@ def extract_train_for_autoML(assets):
             if label["labelType"] == "DEFAULT":
                 jsonResponse = json.loads(label["jsonResponse"])
                 X_train.append(asset["content"])
-                y_train.append(converter_name_to_label[jsonResponse["categories"][0]["name"]])
+                y_train.append(
+                    converter_name_to_label[jsonResponse["categories"][0]["name"]])
     return X_train, y_train
+
 
 def escape_content(str):
     return str.replace('\\', '\\\\').replace('"', '\\"').replace('\n', '\\n').replace('\r', '\\r').replace('\\$', "$")
 
-    
+
 def load_dataset_and_predictions(data_path, figure_path):
     X_train, y_train, X_test, y_test = load_imdb_dataset(data_path)
-    
-    #Explore dataset
-    print_infos_dataset(X_train,y_train,X_test,y_test)
+
+    # Explore dataset
+    print_infos_dataset(X_train, y_train, X_test, y_test)
     plot_sample_length_distribution(figure_path, X_train)
     plot_most_frequent_words_or_ngrams(figure_path, X_train)
 
-
     return X_train, y_train, X_test, y_test
 
+
 def automl_train_and_predict(X_train, y_train, X_to_be_predicted, y_to_be_predicted):
-    x_train, x_to_be_predicted = ngram_vectorize(X_train, y_train, X_to_be_predicted)
+    x_train, x_to_be_predicted = ngram_vectorize(
+        X_train, y_train, X_to_be_predicted)
 
     tmp_folder = './autosklearn_parallel_1_example_tmp'
     output_folder = './autosklearn_parallel_1_example_out'
 
-    cls = autosklearn.classification.AutoSklearnClassifier(n_jobs =4, time_left_for_this_task = 200,
+    cls = autosklearn.classification.AutoSklearnClassifier(n_jobs=4, time_left_for_this_task=200,
                                                            per_run_time_limit=20,
                                                            tmp_folder=tmp_folder,
                                                            output_folder=output_folder,
                                                            seed=10
-                                                        )
+                                                           )
     cls.fit(x_train, y_train)
     assert x_train.shape[1] == x_to_be_predicted.shape[1]
     predictions = cls.predict(x_to_be_predicted)
     print("Accuracy: {}".format(accuracy_score(y_to_be_predicted, predictions)))
-    print("Precision|Recall|F1Score|Support: {}".format(precision_recall_fscore_support(y_to_be_predicted, predictions)))
+    print("Precision|Recall|F1Score|Support: {}".format(
+        precision_recall_fscore_support(y_to_be_predicted, predictions)))
     shutil.rmtree(tmp_folder)
     shutil.rmtree(output_folder)
     return predictions
-
 
 
 @click.command()
@@ -217,20 +234,23 @@ def main(graphql_client):
     if not os.path.exists(plot_path):
         os.mkdir(plot_path)
 
-    mail = input('Enter Email: ')
-    password =getpass.getpass('Enter password for user {}:'.format(mail))
-    client, user_id = authenticate( mail, password, api_endpoint=graphql_client)
+    email = input('Enter Email: ')
+    password = getpass.getpass('Enter password for user {}:'.format(email))
+    kauth = KiliAuth(email, password, api_endpoint=graphql_client)
+    playground = Playground(kauth)
     project_id = input('Enter project id: ')
 
-    X_train_origin, y_train_origin, X_test, y_test = load_dataset_and_predictions(data_path, plot_path)
+    X_train_origin, y_train_origin, X_test, y_test = load_dataset_and_predictions(
+        data_path, plot_path)
 
     # Push dataset on project
     converter_external_id_to_id = {}
     for external_id, content in enumerate(tqdm(X_test)):
         # Insert asset
-        append_to_dataset(
-            client, project_id, escape_content(content), external_id)
-        asset = get_assets_by_external_id(client, project_id, external_id)
+        playground.append_to_dataset(
+            project_id=project_id, content=escape_content(content), external_id=external_id)
+        asset = playground.get_assets_by_external_id(
+            project_id=project_id, external_id=external_id)
         asset_id = asset[0]['id']
         converter_external_id_to_id[external_id] = asset_id
 
@@ -238,18 +258,22 @@ def main(graphql_client):
     STOP_CONDITION = True
     while STOP_CONDITION:
         print("Export assets and labels...")
-        assets = export_assets(client, project_id)
+        assets = playground.export_assets(project_id=project_id)
         print("Done.\n")
         X_train, y_train = extract_train_for_autoML(assets)
 
-        if len(X_train)>5:
+        if len(X_train) > 5:
             print("Online Learning is on its way...")
-            predictions = automl_train_and_predict(X_train, y_train, X_test, y_test)
+            predictions = automl_train_and_predict(
+                X_train, y_train, X_test, y_test)
             # Insert pre-annotations
             for external_id, prediction in enumerate(tqdm(predictions)):
-                json_response = {"categories":[{"name":converter_label_to_name[prediction],"confidence":100}]}
-                create_prediction(client, converter_external_id_to_id[external_id], json_response)
+                json_response = {"categories": [
+                    {"name": converter_label_to_name[prediction], "confidence":100}]}
+                playground.create_prediction(
+                    asset_id=converter_external_id_to_id[external_id], json_response=json_response)
             print("Done.\n")
+
 
 if __name__ == '__main__':
     main()
