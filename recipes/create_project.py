266cb8fa6b18d1de17727b02e375ba4d1fbafb16
Edouard d'Archimbaud <edouard@kili-technology.com>
2019-11-19 16:46:44 +0100

    Fix

--- recipes/create_project.py
+++ recipes/create_project.py
@@ -6,8 +6,12 @@ import click
 import yaml
 from tqdm import tqdm
 
-from kili.authentication import KiliAuth
-from kili.playground import Playground
+from kili.authentication import authenticate
+from kili.mutations.organization import create_organization
+from kili.mutations.project import create_empty_project, update_project, append_to_roles
+from kili.mutations.tool import append_to_tools
+from kili.mutations.user import create_user
+from kili.queries.project import get_projects
 
 
 def get(dic, key):
