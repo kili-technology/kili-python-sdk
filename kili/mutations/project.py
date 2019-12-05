05533cbfbc097317cac7071009fffacfa641ce3f
Edouard d'Archimbaud <edouard@kili-technology.com>
2019-12-05 11:31:07 +0100

    Fix

--- kili/mutations/project.py
+++ kili/mutations/project.py
@@ -1,5 +1,5 @@
 import time
-from json import dumps
+from json import dumps, loads
 
 from tqdm import tqdm
 
@@ -270,7 +270,7 @@ def force_project_kpis(client, project_id):
         unique_asset_authors = list(
             set([label['author']['id'] for label in asset['labels']]))
         update_properties_in_asset(client, asset['id'], external_id=asset['externalId'], priority=asset['priority'],
-                                   json_metadata=json.loads(asset['jsonMetadata']), consensus_mark=asset['calculatedConsensusMark'], honeypot_mark=asset['calculatedHoneypotMark'])
+                                   json_metadata=loads(asset['jsonMetadata']), consensus_mark=asset['calculatedConsensusMark'], honeypot_mark=asset['calculatedHoneypotMark'])
         for asset_author in unique_asset_authors:
             numbers_of_labeled_assets[asset_author] = 1 if asset_author not in numbers_of_labeled_assets else \
                 numbers_of_labeled_assets[asset_author] + 1
