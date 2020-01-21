ff56756e7990cb1486d1feaaeef69b734146a0d9
BruceAtKili <bruce@kili-technology.com>
2020-01-21 11:59:57 +0100

    Adding a recipe

---
+++ recipes/update_project_title.py
@@ -0,0 +1,28 @@
+import getpass
+
+import click
+
+from kili.authentication import KiliAuth
+from kili.playground import Playground
+
+
+@click.command()
+@click.option('--api_endpoint', default='https://cloud.kili-technology.com/api/label/graphql',
+              help='Endpoint of GraphQL client')
+def main(api_endpoint):
+    email = input('Enter email: ')
+    password = getpass.getpass()
+    project_id = input('Enter project id: ')
+    title = input(
+        'Enter a new title (leave blank and press [Enter] to leave title as is): ').strip()
+    description = input(
+        'Enter a new description (leave blank and press [Enter] to leave description as is): ').strip()
+
+    kauth = KiliAuth(email, password, api_endpoint=api_endpoint)
+    playground = Playground(kauth)
+    playground.update_properties_in_project(
+        project_id=project_id, title=title, description=description)
+
+
+if __name__ == '__main__':
+    main()
