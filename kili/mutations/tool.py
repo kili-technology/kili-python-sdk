d2925b0eb8d83b7307316618f7ef60dabca57fcb
Edouard d'Archimbaud <edouard@kili-technology.com>
2020-02-05 19:01:29 +0100

    Removed tool name

--- kili/mutations/tool.py
+++ kili/mutations/tool.py
@@ -1,33 +1,31 @@
 from ..helpers import format_result, json_escape
 
 
-def update_tool(client, tool_id: str, project_id: str, name: str, tool_type: str, json_settings: dict):
+def update_tool(client, tool_id: str, project_id: str, tool_type: str, json_settings: dict):
     result = client.execute('''
     mutation {
       updateTool(toolID: "%s",
         projectID: "%s",
-        name: "%s",
         toolType: %s,
         jsonSettings: "%s") {
           id
       }
     }
-    ''' % (tool_id, project_id, name, tool_type, json_escape(json_settings)))
+    ''' % (tool_id, project_id, tool_type, json_escape(json_settings)))
     return format_result('updateTool', result)
 
 
-def append_to_tools(client, project_id: str, name: str, tool_type: str, json_settings: dict):
+def append_to_tools(client, project_id: str,  tool_type: str, json_settings: dict):
     result = client.execute('''
     mutation {
       appendToTools(
         projectID: "%s",
-        name: "%s",
         toolType: %s,
         jsonSettings: "%s") {
           id
       }
     }
-    ''' % (project_id, name, tool_type, json_escape(json_settings)))
+    ''' % (project_id,  tool_type, json_escape(json_settings)))
     return format_result('appendToTools', result)
 
 
