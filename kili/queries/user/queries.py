cfd9568a61fc3edb6c6765cff14e171aca51ee16
Pierre Marcenac <pierre@kili-technology.com>
2020-04-17 18:07:18 +0200

    Fix query on "users"

--- kili/queries/user/queries.py
+++ kili/queries/user/queries.py
@@ -1,7 +1,7 @@
 from .fragments import USER_FRAGMENT
 
 GQL_USERS = f'''
-query($where: UserWhere!, $first: PageSize!, skip: Int!) {{
+query($where: UserWhere!, $first: PageSize!, $skip: Int!) {{
   data: users(where: $where, first: $first, skip: $skip) {{
     {USER_FRAGMENT}
   }}
