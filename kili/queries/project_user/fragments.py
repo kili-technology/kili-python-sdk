45bea1c9861ede419e670ee6677e4026baa71b4b
Pierre Marcenac <pierre@kili-technology.com>
2020-04-21 18:06:03 +0200

    Add new queries for projectUsers

---
+++ kili/queries/project_user/fragments.py
@@ -0,0 +1,27 @@
+PROJECT_USER_FRAGMENT = '''
+id
+activated
+role
+user {
+  id
+  email
+}
+'''
+
+PROJECT_USER_FRAGMENT_WITH_KPIS = '''
+id
+activated
+consensusMark
+honeypotMark
+lastLabelingAt
+numberOfAnnotations
+numberOfLabeledAssets
+numberOfLabels
+role
+starred
+totalDuration
+user {
+  id
+  email
+}
+'''
