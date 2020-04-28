43822e8d8ed2e7059a4c88841b5952893b432a3a
Edouard d'Archimbaud <edouard@kili-technology.com>
2020-04-28 16:30:42 +0200

    Deprecated old label queries

--- kili/queries/label/__init__.py
+++ kili/queries/label/__init__.py
@@ -1,28 +1,51 @@
 import pandas as pd
 
-from ...helpers import format_result
+from ...helpers import deprecate, format_result
 from ..asset import assets
 from ..project import get_project
-from .queries import (GQL_GET_LABEL, GQL_GET_LATEST_LABELS,
-                      GQL_GET_LATEST_LABELS_FOR_USER)
+from .queries import GQL_LABELS
 
 
+def labels(client, asset_id: str = None, user_id: str = None, skip: int = 0, first: int = None):
+    formatted_first = first if first else 100
+    variables = {
+        'assetID': asset_id,
+        'userID': user_id,
+        'skip': skip,
+        'first': formatted_first
+    }
+    result = client.execute(GQL_LABELS, variables)
+    return format_result('data', result)
+
+
+@deprecate(
+    """
+        This function is deprecated. get_label used to fetch labels from an asset_id and a user_id. It is now achievable with labels.
+        To fetch labels from an asset_id and a user_id, use:
+            > playground.labels(asset_id=asset_id, user_id=user_id)
+        """)
 def get_label(client, asset_id: str, user_id: str):
-    variables = {'assetID': asset_id, 'userID': user_id}
-    result = client.execute(GQL_GET_LABEL, variables)
-    return format_result('data', result)
+    return None
 
 
+@deprecate(
+    """
+        This function is deprecated. get_latest_labels_for_user used to fetch labels from a project_id and a user_id. It is now achievable with labels.
+        To fetch labels from a project_id and a user_id, use:
+            > playground.labels(project_id=project_id, user_id=user_id)
+        """)
 def get_latest_labels_for_user(client, project_id: str, user_id: str):
-    variables = {'projectID': project_id, 'userID': user_id}
-    result = client.execute(GQL_GET_LATEST_LABELS_FOR_USER, variables)
-    return format_result('data', result)
+    return None
 
 
+@deprecate(
+    """
+        This function is deprecated. get_latest_labels used to fetch labels from a project_id. It is now achievable with labels.
+        To fetch labels from a project_id, use:
+            > playground.labels(project_id=project_id)
+        """)
 def get_latest_labels(client, project_id: str, skip: int, first: int):
-    variables = {'projectID': project_id, 'skip': skip, 'first': first}
-    result = client.execute(GQL_GET_LATEST_LABELS, variables)
-    return format_result('data', result)
+    return None
 
 
 def parse_json_response_for_single_classification(json_response):
@@ -56,9 +79,9 @@ def export_labels_as_df(client, project_id: str):
         return pd.DataFrame()
 
     interface_category = project['interfaceCategory']
-    assets = assets(client, project_id=project_id)
+    _assets = assets(client, project_id=project_id)
     labels = [dict(label, **dict((f'asset__{key}', asset[key]) for key in asset))
-              for asset in assets for label in asset['labels']]
+              for asset in _assets for label in asset['labels']]
     labels_df = pd.DataFrame(labels)
     labels_df['y'] = labels_df['jsonResponse'].apply(
         lambda json_response: parse_json_response(json_response, interface_category))
