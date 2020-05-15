5c5912084463f9ae0f2a9cb1721a4c3c82df5f4d
philippe-perso <philstc95@gmail.com>
2020-05-15 20:04:33 +0200

    Revert modification to just deprecate fragment

--- kili/queries/asset/fragments.py
+++ kili/queries/asset/fragments.py
@@ -43,3 +43,26 @@ locks {{
   {LOCK_FRAGMENT}
 }}
 '''
+
+ASSET_FRAGMENT_SIMPLIFIED = f'''
+id
+content
+externalId
+jsonMetadata
+createdAt
+updatedAt
+isHoneypot
+status
+labels {{
+  id
+  author {{
+    {ASSET_FRAGMENT_AUTHOR}
+  }}
+  labelType
+  jsonResponse
+  createdAt
+  secondsToLabel
+  totalSecondsToLabel
+  honeypotMark
+  isLatestLabelForUser
+}}
