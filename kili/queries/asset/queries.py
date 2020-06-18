84aab46d0bec1e052c252ea79e5776c386b53bbe
mduval <maxime@kili-technology.com>
2020-06-18 18:43:37 +0200

    Fix query when no field label. is used

--- kili/queries/asset/queries.py
+++ kili/queries/asset/queries.py
@@ -1,11 +1,19 @@
 def gql_assets(fragment, fragment_label):
-    return f'''
-query($where: AssetWhere!, $first: PageSize!, $skip: Int!, $labelWhere: LabelWhere!) {{
-  data: assets(where: $where, skip: $skip, first: $first) {{
-    {fragment}
+    if len(fragment_label) == 0:
+        query = 'query($where: AssetWhere!, $first: PageSize!, $skip: Int!)'
+        query_labels = ''
+    else:
+        query = 'query($where: AssetWhere!, $first: PageSize!, $skip: Int!, $labelWhere: LabelWhere!)'
+        query_labels = f'''
     labels: labelsWhere(where: $labelWhere, skip: 0, first: 100) {{
         {fragment_label}
     }}
+'''
+    return f'''
+{query} {{
+  data: assets(where: $where, skip: $skip, first: $first) {{
+    {fragment}
+    {query_labels}
   }}
 }}
 '''
