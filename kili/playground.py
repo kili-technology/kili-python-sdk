fb9c3c4c562c4a4abd385d923f49cdf7705460ab
philippe-perso <philstc95@gmail.com>
2020-08-12 11:14:01 +0200

    Fix import missing for lock queries

--- kili/playground.py
+++ kili/playground.py
@@ -6,6 +6,7 @@ import kili.mutations.project
 import kili.mutations.user
 import kili.queries.asset
 import kili.queries.label
+import kili.queries.lock
 import kili.queries.organization
 import kili.queries.project
 import kili.queries.project_user
