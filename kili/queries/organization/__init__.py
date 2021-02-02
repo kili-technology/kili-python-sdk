960bfa6df70abe40f5acc57e4ded046d54469a08
Edouard d'Archimbaud <edouard@kili-technology.com>
2021-02-02 12:05:54 +0100

    Added countOrganizations

--- kili/queries/organization/__init__.py
+++ kili/queries/organization/__init__.py
@@ -1,7 +1,7 @@
 import warnings
 
 from ...helpers import Compatible, deprecate, format_result, fragment_builder
-from .queries import gql_organizations
+from .queries import gql_organizations, GQL_ORGANIZATIONS_COUNT
 from ...types import Organization
 
 
@@ -53,4 +53,34 @@ class QueriesOrganization:
         GQL_ORGANIZATIONS = gql_organizations(
             fragment_builder(fields, Organization))
         result = self.auth.client.execute(GQL_ORGANIZATIONS, variables)
+        return format_result('data', result)
+    
+    @Compatible(['v2'])
+    def count_organizations(self, email: str = None, organization_id: str = None):
+        """
+        Count organizations
+
+        Returns users whose email / organization id correspond to the given ones.
+
+        Parameters
+        ----------
+        - email : str, optional (default = None)
+        - organization_id : str, optional (default = None)
+        - fields : list of string, optional (default = ['id', 'name'])
+            All the fields to request among the possible fields for the organizations, default for None are the non-calculated fields)
+            Possible fields : see https://cloud.kili-technology.com/docs/python-graphql-api/graphql-api/#organization
+
+        Returns
+        -------
+        - a result object which contains the query if it was successful, or an error message else.
+        """
+        variables = {
+            'where': {
+                'id': organization_id,
+                'user': {
+                    'email': email,
+                }
+            }
+        }
+        result = self.auth.client.execute(GQL_ORGANIZATIONS_COUNT, variables)
         return format_result('data', result)
