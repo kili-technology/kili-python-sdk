5ddd8da23ad09545eed41ecdfbb7bb67e7bea34c
mduval <maxime@kili-technology.com>
2021-12-08 14:52:36 +0100

    Test all recipes

---
+++ .github/workflows/TESTING.md
@@ -0,0 +1,28 @@
+# Testing the repo
+
+
+## Pylint
+
+- Go to the folder repository.
+- `pip install pylint`
+- `pylint kili`
+
+## Recipes
+
+```bash
+# Export the right environment variables 
+export KILI_API_ENDPOINT=https://staging.cloud.kili-technology.com/api/label/v2/graphql
+export KILI_USER_API_KEY=...
+export KILI_USER_EMAIL=...
+export KILI_USER_ID=...
+
+# Install dependencies
+pip install jupyter nbconvert nbformat kili pytest
+
+# Run pytest
+pytest
+```
+
+**Add a recipe to the list of tested recipes**
+
+Add an entry to `RECIPES_TESTED` in `test_notebooks.py`.
--- test/test_notebooks.py
+++ test/test_notebooks.py
@@ -11,7 +11,17 @@ from nbconvert.preprocessors import CellExecutionError
 
 RECIPES_DIR = './recipes'
 RECIPES_TESTED = [
-    'import_predictions.ipynb'
+    'import_predictions.ipynb',
+    'create_project.ipynb',
+    'medical_imaging.ipynb',
+    'query_methods.ipynb',
+    'import_assets.ipynb',
+    'pixel_level_masks.ipynb',
+    'import_text_assets.ipynb',
+    'export_labels.ipynb',
+    'inference_labels.ipynb',
+    'getting-started/getting_started-image_bounding_box_detection.ipynb',
+    'getting-started/getting_started-image_classification.ipynb',
 ]
 
 def process_notebook(notebook_filename):
