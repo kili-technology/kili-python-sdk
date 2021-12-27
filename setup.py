de54d8468956452e58aacd686de36c0719b3a1e8
mduval <maxime@kili-technology.com>
2021-12-27 22:24:29 +0100

    Don't ask for specific version of pandas

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ setup(
 
     long_description_content_type="text/markdown",
 
-    install_requires=["pandas==1.2.5",
+    install_requires=["pandas",
                       "requests",
                       "six",
                       "tqdm",
@@ -55,6 +55,9 @@ setup(
                  'Programming Language :: Python :: 3.5',
                  'Programming Language :: Python :: 3.6',
                  'Programming Language :: Python :: 3.7',
+                 'Programming Language :: Python :: 3.8',
+                 'Programming Language :: Python :: 3.9',
+                 'Programming Language :: Python :: 3.10',
                  ('Programming Language :: Python :: '
                   'Implementation :: CPython'),
                  ('Programming Language :: Python :: '
