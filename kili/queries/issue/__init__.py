39647fcd1db22025d1cd77bb2c213f31c5bf4e1c
Theo Dullin <theo@kili-technology.com>
2022-04-12 15:31:54 +0200

    feat(ML-164): update issues query

--- kili/queries/issue/__init__.py
+++ kili/queries/issue/__init__.py
@@ -7,9 +7,12 @@ from typing import List, Optional
 
 from typeguard import typechecked
 
+from kili.utils import row_generator_from_paginated_calls
+
 from ...helpers import Compatible, format_result, fragment_builder
-from .queries import gql_issues
+from .queries import GQL_ISSUES_COUNT, gql_issues
 from ...types import Issue as IssueType
+
 
 @dataclass
 class QueriesIssue:
@@ -41,14 +44,26 @@ class QueriesIssue:
                    'type'],
                first: Optional[int] = 100,
                project_id: Optional[str] = None,
-               skip: Optional[int] = 0):
+               skip: Optional[int] = 0,
+               disable_tqdm: bool = False,
+               as_generator: bool = False):
         """
         Get an array of issues given a set of criteria
 
         Parameters
         ----------
+        - fields : list of string, optional (default = ['id', 'createdAt', 'hasBeenSeen', 'status', 'type'])
+            All the fields to request among the possible fields for the assets.
+            See [the documentation](https://cloud.kili-technology.com/docs/python-graphql-api/graphql-api/#issue) for all possible fields.
+        - first : int, optional (default = None)
+            Maximum number of issues to return.
         - project_id : str, optional (default = None)
             Project ID the issue belongs to.
+        - skip : int, optional (default = None)
+            Number of issues to skip (they are ordered by their date of creation, first to last).
+        - disable_tqdm : bool, (default = False)
+        - as_generator: bool, (default = False)
+            If True, a generator on the API key is returned.
 
         Returns
         -------
@@ -59,7 +74,9 @@ class QueriesIssue:
         >>> # List all issues of a project and their authors
         >>> kili.issues(project_id=project_id, fields=['author.email'])
         """
-        variables = {
+        count_args = {'project_id': project_id}
+        disable_tqdm = disable_tqdm or as_generator
+        payload_query = {
             'where': {
                 'project': {
                     'id': project_id,
@@ -68,6 +85,55 @@ class QueriesIssue:
             'skip': skip,
             'first': first,
         }
+
+        issues_generator = row_generator_from_paginated_calls(
+            skip,
+            first,
+            self.count_issues,
+            count_args,
+            self._query_issues,
+            payload_query,
+            fields,
+            disable_tqdm
+        )
+
+        if as_generator:
+            return issues_generator
+        return list(issues_generator)
+
+    def _query_issues(self,
+                      skip: int,
+                      first: int,
+                      payload: dict,
+                      fields: List[str]):
+        payload.update({'skip': skip, 'first': first})
         _gql_issues = gql_issues(fragment_builder(fields, IssueType))
-        result = self.auth.client.execute(_gql_issues, variables)
+        result = self.auth.client.execute(_gql_issues, payload)
         return format_result('data', result)
+
+    @Compatible(['v2'])
+    @typechecked
+    def count_issues(self, project_id: Optional[str] = None):
+        """
+        Count and return the number of api keys with the given constraints
+
+        Parameters
+        ----------
+        - project_id : str, optional (default = None)
+            Project ID the issue belongs to.
+
+        Returns
+        -------
+        - the number of issues with the parameters provided
+
+        """
+        variables = {
+            'where': {
+                'project': {
+                    'id': project_id,
+                },
+            },
+        }
+        result = self.auth.client.execute(GQL_ISSUES_COUNT, variables)
+        count = format_result('data', result)
+        return count
