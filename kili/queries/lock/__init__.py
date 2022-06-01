def22eb63ce2c0599fbf89fbefdd13fb309aef0e
Pierre Leveau <pierre.leveau@kili-technology.com>
2022-06-01 22:56:39 +0200

    fix: lock query types

--- kili/queries/lock/__init__.py
+++ kili/queries/lock/__init__.py
@@ -91,7 +91,7 @@ class QueriesLock:
 
     @Compatible(['v1', 'v2'])
     @typechecked
-    def count_locks(self: any) -> int:
+    def count_locks(self) -> int:
         """Get the number of locks
 
         Args:
