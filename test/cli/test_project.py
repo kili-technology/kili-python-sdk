c8be90505dd77d82ecd1f2ed0d871126d8659486
Florian_Le_Roy <florian.leroy@kili-technology.com>
2022-07-11 18:05:49 +0200

    add test for cli project member update

--- test/cli/test_project.py
+++ test/cli/test_project.py
@@ -13,6 +13,8 @@ from kili.cli.project.list_ import list_projects
 from kili.cli.project.member.list_ import list_members
 from kili.cli.project.member.add import add_member
 from kili.cli.project.member.remove import remove_member
+from kili.cli.project.member.update import update_member
+
 
 from ..utils import debug_subprocess_pytest
 
@@ -22,38 +24,38 @@ def mocked__project_users(project_id=None, **_):
         return [{'activated': True,
                 'id': 'role_id_1',
                  'role': 'ADMIN',
-                 'user': {'email': 'alice@test.com',
-                          'firstname': 'alice',
-                          'id': 'user_id',
-                          'lastname': 'alice',
-                          'organization': {'name': 'test'}}},
-                {'activated': True,
-                'id': 'role_id_2',
-                 'role': 'LABELER',
-                 'user': {'email': 'bob@test.com',
-                          'firstname': 'bob',
-                          'id': 'user_id_2',
-                          'lastname': 'bob',
-                          'organization': {'name': 'test'}}}, ]
-    elif project_id == 'new_project':
-        return[]
-    else:
-        return [{'activated': True,
-                'id': 'role_id_1',
-                 'role': 'ADMIN',
                  'user': {'email': 'john.doe@test.com',
                           'firstname': 'john',
-                          'id': 'user_id',
+                          'id': 'user_id_1',
                           'lastname': 'doe',
                           'organization': {'name': 'test'}}},
                 {'activated': True,
                 'id': 'role_id_2',
+                 'role': 'REVIEWER',
+                 'user': {'email': 'jane.doe@test.com',
+                          'firstname': 'jane',
+                          'id': 'user_id_2',
+                          'lastname': 'doe',
+                          'organization': {'name': 'test'}}}, ]
+    elif project_id == 'project_id_dest':
+        return [{'activated': True,
+                'id': 'role_id_3',
+                 'role': 'LABELER',
+                 'user': {'email': 'john.doe@test.com',
+                          'firstname': 'john',
+                          'id': 'user_id_1',
+                          'lastname': 'doe',
+                          'organization': {'name': 'test'}}},
+                {'activated': True,
+                'id': 'role_id_4',
                  'role': 'LABELER',
                  'user': {'email': 'jane.doe@test.com',
                           'firstname': 'jane',
                           'id': 'user_id_2',
                           'lastname': 'doe',
                           'organization': {'name': 'test'}}}, ]
+    else:
+        return[]
 
 
 def mocked__projects(project_id=None, **_):
@@ -98,6 +100,7 @@ kili_client.project_users = project_users_mock = MagicMock(
     side_effect=mocked__project_users)
 kili_client.append_to_roles = append_to_roles_mock = MagicMock()
 kili_client.delete_from_roles = delete_from_roles_mock = MagicMock()
+kili_client.update_properties_in_role = update_properties_in_role_mock = MagicMock()
 
 kili = MagicMock()
 kili.Kili = MagicMock(return_value=kili_client)
@@ -314,7 +317,8 @@ class TestCLIProject():
 
     def test_list_members(self, mocker):
         runner = CliRunner()
-        result = runner.invoke(list_members, ['--project-id', "project_id"])
+        result = runner.invoke(
+            list_members, ['--project-id', "project_id_dest"])
         debug_subprocess_pytest(result)
         assert ((result.output.count("Jane Doe") == 1) and
                 (result.output.count("@test.com") == 2))
@@ -322,13 +326,13 @@ class TestCLIProject():
     def test_add_member(self, mocker):
         TEST_CASES = [{
             'case_name': 'AAU, when I add one user with email adreses, I see a success',
-            'inputs': ['bob@test.com', 'alice@test.com'],
+            'inputs': ['jane.doe@test.com', 'john.doe@test.com'],
             'options': {
                 'project-id': 'new_project',
             },
             'expected_mutation_payload': {
                 'project_id': 'new_project',
-                'user_email': 'alice@test.com',
+                'user_email': 'john.doe@test.com',
                 'role': 'LABELER',
             }
         },
@@ -340,8 +344,8 @@ class TestCLIProject():
             },
             'expected_mutation_payload': {
                 'project_id': 'new_project',
-                'user_email': 'bob@test.com',
-                'role': 'LABELER',
+                'user_email': 'jane.doe@test.com',
+                'role': 'REVIEWER',
             }
         },
             {
@@ -353,7 +357,7 @@ class TestCLIProject():
             },
             'expected_mutation_payload': {
                 'project_id': 'new_project',
-                'user_email': 'bob@test.com',
+                'user_email': 'john.doe@test.com',
                 'role': 'REVIEWER',
             }
         },
@@ -363,8 +367,8 @@ class TestCLIProject():
             # pylint: disable=unspecified-encodind
             with open('user_list.csv', 'w') as f:
                 writer = csv.writer(f)
-                writer.writerow(['alice@test.com'])
-                writer.writerow(['bob@test.com'])
+                writer.writerow(['jane.doe@test.com'])
+                writer.writerow(['john.doe@test.com'])
 
             for i, test_case in enumerate(TEST_CASES):
                 print(test_case['case_name'])
@@ -381,32 +385,32 @@ class TestCLIProject():
     def test_remove_member(self, mocker):
         TEST_CASES = [{
             'case_name': 'AAU, when I remove one user with email adresses, I see a success',
-            'inputs': ['bob@test.com', 'alice@test.com'],
+            'inputs': ['jane.doe@test.com', 'john.doe@test.com'],
             'options': {
-                'project-id': 'project_id_source',
+                'project-id': 'project_id_dest',
             },
             'expected_mutation_payload': {
-                'role_id': 'role_id_1',
+                'role_id': 'role_id_3',
             }
         },
             {
             'case_name': 'AAU, when I remove all users, I see a success',
             'inputs': ['all'],
             'options': {
-                'project-id': 'project_id_source',
+                'project-id': 'project_id_dest',
             },
             'expected_mutation_payload': {
-                'role_id': 'role_id_1',
+                'role_id': 'role_id_3',
             }
         },
             {
             'case_name': 'AAU, when I remove user with csv file, I see a success',
             'inputs': ['user_list.csv'],
             'options': {
-                'project-id': 'project_id_source',
+                'project-id': 'project_id_dest',
             },
             'expected_mutation_payload': {
-                'role_id': 'role_id_1',
+                'role_id': 'role_id_3',
             }
         },
         ]
@@ -415,8 +419,8 @@ class TestCLIProject():
             # pylint: disable=unspecified-encodind
             with open('user_list.csv', 'w') as f:
                 writer = csv.writer(f)
-                writer.writerow(['alice@test.com'])
-                writer.writerow(['bob@test.com'])
+                writer.writerow(['jane.doe@test.com'])
+                writer.writerow(['john.doe@test.com'])
 
             for i, test_case in enumerate(TEST_CASES):
                 print(test_case['case_name'])
@@ -428,4 +432,66 @@ class TestCLIProject():
                 debug_subprocess_pytest(result)
                 assert delete_from_roles_mock.call_count == 2 * (i+1)
                 delete_from_roles_mock.assert_called_with(
+                    **test_case['expected_mutation_payload'])
+
+    def test_update_member(self, mocker):
+        TEST_CASES = [{
+            'case_name': "AAU, when I update user's role with email adresses, I see a success",
+            'inputs': ['jane.doe@test.com', 'john.doe@test.com'],
+            'options': {
+                'project-id': 'project_id_dest',
+                'role': 'TEAM_MANAGER'
+            },
+            'expected_mutation_payload': {
+                'role_id': 'role_id_3',
+                'project_id': 'project_id_dest',
+                'user_id': 'user_id_1',
+                'role': 'TEAM_MANAGER',
+            }
+        },
+            {
+            'case_name': "AAU, when I update user's role from another project, I see a success",
+            'inputs': ['project_id_source'],
+            'options': {
+                'project-id': 'project_id_dest',
+            },
+            'expected_mutation_payload': {
+                'role_id': 'role_id_3',
+                'project_id': 'project_id_dest',
+                'user_id': 'user_id_1',
+                'role': 'REVIEWER',
+            }
+        },
+            {
+            'case_name': "AAU, when I update user's role with csv file, I see a success",
+            'inputs': ['user_list.csv'],
+            'options': {
+                'project-id': 'project_id_dest',
+            },
+            'expected_mutation_payload': {
+                'role_id': 'role_id_3',
+                'project_id': 'project_id_dest',
+                'user_id': 'user_id_1',
+                'role': 'ADMIN',
+            }
+        },
+        ]
+        runner = CliRunner()
+        with runner.isolated_filesystem():
+            # pylint: disable=unspecified-encodind
+            with open('user_list.csv', 'w') as f:
+                writer = csv.writer(f)
+                writer.writerow(['alice@test.com', 'REVIEWER'])
+                writer.writerow(['bob@test.com', 'ADMIN'])
+
+            for i, test_case in enumerate(TEST_CASES):
+                print(test_case['case_name'])
+                arguments = test_case['inputs']
+                for k, v in test_case['options'].items():
+                    arguments.append('--'+k)
+                    arguments.append(v)
+                result = runner.invoke(update_member, arguments)
+                debug_subprocess_pytest(result)
+                assert update_properties_in_role_mock.call_count == 2 * (i+1)
+                update_properties_in_role_mock.assert_called_with(
                     **test_case['expected_mutation_payload'])
