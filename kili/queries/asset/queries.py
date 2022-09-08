31527044679fafae3aac19243d940e9d3945f2d4
Theo Dullin <theo@kili-technology.com>
2022-09-08 17:19:03 +0200

    feat: implement signed url upload from SDK

--- kili/queries/asset/queries.py
+++ kili/queries/asset/queries.py
@@ -21,3 +21,9 @@ query($where: AssetWhere!) {
   data: countAssets(where: $where)
 }
 """
+
+GQL_CREATE_UPLOAD_BUCKET_SIGNED_URLS = """
+query($projectID: ID!, $size: Int) {
+  urls: createUploadBucketSignedUrls(projectID: $projectID, size: $size)
+}
+"""
