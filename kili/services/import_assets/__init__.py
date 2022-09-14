ec5ffbdfc3759ed1360fbd88f1b72ba8758ac246
Theo Dullin <theo@kili-technology.com>
2022-09-14 11:19:39 +0200

    feat: rename import_assets_services

--- kili/services/import_assets/__init__.py
+++ kili/services/import_assets/__init__.py
@@ -9,7 +9,7 @@ from kili.services.import_assets.legacy import LegacyImporter
 from kili.services.import_assets.types import AssetToImport
 
 
-def import_assets_service(
+def import_assets(
     auth: KiliAuth,
     project_id: str,
     assets: List[AssetToImport],
@@ -21,6 +21,7 @@ def import_assets_service(
     projects = kili.projects(project_id, disable_tqdm=True)
     assert len(projects) == 1, NO_ACCESS_RIGHT
     input_type = projects[0]["inputType"]
+
     legacy_importer = LegacyImporter(
         auth=auth, project_id=project_id, input_type=input_type, assets=assets
     )
