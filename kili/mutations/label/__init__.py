1a8d6218bca9539ae2dff0e7465605e6c98f87fe
gheorghetutunaru <99664787+gheorghetutunaru@users.noreply.github.com>
2022-09-21 16:14:46 +0200

    chore(DATA-671): remove skipped from append_to_labels mutation (#673)

--- kili/mutations/label/__init__.py
+++ kili/mutations/label/__init__.py
@@ -135,8 +135,6 @@ class MutationsLabel:
                 "jsonResponse": dumps(json_response),
                 "labelType": label_type,
                 "secondsToLabel": seconds_to_label,
-                "skipped": False,  # this is a deprecated field but kept here for compatibility
-                # with deployed backends.
             },
             "where": {"id": label_asset_id},
         }
