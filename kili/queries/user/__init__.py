cb7d2e93cc8b2120090a7f78267c43ed7efd5568
charbel-raphael.segerie <charbel-raphael.segerie@kili-technology.com>
2022-09-21 16:53:26 +0200

    chore: correct type of count functions

--- kili/queries/user/__init__.py
+++ kili/queries/user/__init__.py
@@ -92,7 +92,6 @@ class QueriesUser:
         return list(users_generator)
 
     def _query_users(self, skip: int, first: int, payload: dict, fields: List[str]):
-
         payload.update({"skip": skip, "first": first})
         _gql_users = gql_users(fragment_builder(fields, User))
         result = self.auth.client.execute(_gql_users, payload)
