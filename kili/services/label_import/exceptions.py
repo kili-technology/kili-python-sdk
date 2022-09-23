46492aa0fea44fc0d401e285f2f73b70192ed444
Pierre Leveau <pierre.leveau@kili-technology.com>
2022-09-23 16:13:37 +0200

    added exceptions

---
+++ kili/services/label_import/exceptions.py
@@ -0,0 +1,27 @@
+"""
+Label import-specific errors
+"""
+
+
+class LabelParsingError(ValueError):
+    """
+    Raised when there is a failure to parse a label
+    """
+
+
+class MissingMetadataError(ValueError):
+    """
+    Raised when a metadata file is missing
+    """
+
+
+class MissingTargetJobError(ValueError):
+    """
+    Raised when the target job is missing
+    """
+
+
+class MissingExternalIdError(ValueError):
+    """
+    Raised when the external id of the labels are missing
+    """
