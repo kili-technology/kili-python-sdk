f64fb98e127718ea669618b50d18f0c5d06d77f0
Pierre Leveau <pierre.leveau@kili-technology.com>
2022-09-28 11:02:18 +0200

    rm more code

--- kili/services/export/format/yolo/common.py
+++ kili/services/export/format/yolo/common.py
@@ -14,60 +14,6 @@ from kili.services.export.types import JobCategory, LabelFormat, YoloAnnotation
 from kili.utils.tqdm import tqdm
 
 
-class LabelFrames:
-    """
-    Holds asset frames data.
-    """
-
-    @staticmethod
-    def from_asset(asset, job_ids) -> "LabelFrames":
-        """
-        Instantiate the label frames from the asset. It handles the case when there are several
-        frames by label or a single one.
-        """
-        frames = {}
-        number_of_frames = 0
-        is_frame_group = False
-        if "jsonResponse" in asset["latestLabel"]:
-            number_of_frames = len(asset["latestLabel"]["jsonResponse"])
-            for idx in range(number_of_frames):
-                if str(idx) in asset["latestLabel"]["jsonResponse"]:
-                    is_frame_group = True
-                    frame_asset = asset["latestLabel"]["jsonResponse"][str(idx)]
-                    for job_id in job_ids:
-                        if (
-                            job_id in frame_asset
-                            and "annotations" in frame_asset[job_id]
-                            and frame_asset[job_id]["annotations"]
-                        ):
-                            frames[idx] = {"latestLabel": {"jsonResponse": frame_asset}}
-                            break
-
-        if not frames:
-            frames[-1] = asset
-        return LabelFrames(frames, number_of_frames, is_frame_group, asset["externalId"])
-
-    def __init__(
-        self, frames: Dict[int, Dict], number_frames: int, is_frame_group: bool, external_id: str
-    ) -> None:
-        self.frames: Dict[int, Dict] = frames
-        self.number_frames: int = number_frames
-        self.is_frame_group: bool = is_frame_group
-        self.external_id: str = external_id
-
-    def get_leading_zeros(self) -> int:
-        """
-        Get leading zeros for file name building
-        """
-        return len(str(self.number_frames))
-
-    def get_label_filename(self, idx: int) -> str:
-        """
-        Get label filemame for index
-        """
-        return f"{self.external_id}_{str(idx + 1).zfill(self.get_leading_zeros())}"
-
-
 class YoloExporter(BaseExporter):
     """
     Common code for Yolo exporters.
@@ -83,5 +29,3 @@ class YoloExporter(BaseExporter):
         """
         with open(output_file, "w") as f:
             f.write(json.dumps(assets, sort_keys=True, indent=4))
-
-
