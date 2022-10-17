fdb981cf869548e39379b05a04cedc3239be9b67
Paul Maisondieu <paul.maisondieu@kili-technology.com>
2022-10-17 15:31:39 +0200

    chore(graphql): remove useless project_id

--- tests/services/asset_import/mocks.py
+++ tests/services/asset_import/mocks.py
@@ -2,7 +2,7 @@
 from unittest.mock import MagicMock
 
 mocked_request_signed_urls = MagicMock(
-    side_effect=lambda _auth, _project_id, size: ["https://signed_url?id=id"] * size
+    side_effect=lambda _auth, size: ["https://signed_url?id=id"] * size
 )
 
 mocked_upload_data_via_rest = MagicMock(side_effect=lambda signed_urls, _a, _b: signed_urls)
