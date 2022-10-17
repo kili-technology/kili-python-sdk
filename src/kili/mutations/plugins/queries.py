a9a71db6c35a535085a03af7c63b8bb3f48fdfe2
Paul Maisondieu <paul.maisondieu@kili-technology.com>
2022-10-17 13:56:00 +0200

    feat(plugins): create beta resolvers

---
+++ src/kili/mutations/plugins/queries.py
@@ -0,0 +1,49 @@
+"""
+Queries of plugins mutations
+"""
+
+PROJECT_FRAGMENT_ID = """
+id
+"""
+
+GQL_UPLOAD_PLUGIN_BETA = """
+mutation(
+  $pluginName: String!
+  $pluginSrc: String!
+  ) {
+  data: uploadPlugin(
+    data: {
+      pluginName: $pluginName
+      pluginSrc: $pluginSrc
+    }
+  )
+}
+"""
+
+GQL_ACTIVATE_PLUGIN_ON_PROJECT = """
+mutation(
+  $pluginName: String!
+  $projectId: ID!
+  ) {
+  data: activatePluginOnProject(
+    where: {
+      pluginName: $pluginName
+      projectId: $projectId
+    }
+  )
+}
+"""
+
+GQL_DEACTIVATE_PLUGIN_ON_PROJECT = """
+mutation(
+  $pluginName: String!
+  $projectId: ID!
+  ) {
+  data: deactivatePluginOnProject(
+    where: {
+      pluginName: $pluginName
+      projectId: $projectId
+    }
+  )
+}
+"""
