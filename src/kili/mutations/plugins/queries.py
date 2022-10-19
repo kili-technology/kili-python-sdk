a199f0d7b9e5ebaf2a6703214489f1bd354a5af1
Paul Maisondieu <paul.maisondieu@kili-technology.com>
2022-10-19 15:25:02 +0200

    feat(plugins): migrate upload to service

--- src/kili/mutations/plugins/queries.py
+++ src/kili/mutations/plugins/queries.py
@@ -2,20 +2,6 @@
 Queries of plugins mutations
 """
 
-GQL_UPLOAD_PLUGIN_BETA = """
-mutation(
-  $pluginName: String!
-  $pluginSrc: String!
-  ) {
-  data: uploadPlugin(
-    data: {
-      pluginName: $pluginName
-      pluginSrc: $pluginSrc
-    }
-  )
-}
-"""
-
 GQL_ACTIVATE_PLUGIN_ON_PROJECT = """
 mutation(
   $pluginName: String!
