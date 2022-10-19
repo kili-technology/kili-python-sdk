03e7cdae77c4695ccc1002f1ddf04b867da141b1
Paul Maisondieu <paul.maisondieu@kili-technology.com>
2022-10-19 15:37:16 +0200

    feat(plugins): migrate activate & deactivate

--- src/kili/services/plugins/__init__.py
+++ src/kili/services/plugins/__init__.py
@@ -1,5 +1,6 @@
 """Service for managing plugins in kili"""
 
+from kili.services.plugins.activation import activate_plugin, deactivate_plugin
 from kili.services.plugins.upload import PluginUploader
 
-__all__ = ["PluginUploader"]
+__all__ = ["PluginUploader", "activate_plugin", "deactivate_plugin"]
