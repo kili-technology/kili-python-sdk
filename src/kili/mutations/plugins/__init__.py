8043e6df45cc7f790b9cd4523019e26be19d1fa8
Paul Maisondieu <paul.maisondieu@kili-technology.com>
2022-10-19 15:25:02 +0200

    feat(plugins): migrate upload to service

--- src/kili/mutations/plugins/__init__.py
+++ src/kili/mutations/plugins/__init__.py
@@ -1,17 +1,15 @@
 """Project mutations."""
 
-from pathlib import Path
+
 from typing import Optional
 
 from typeguard import typechecked
 
+from kili.services.plugins import PluginUploader
+
 from ...authentication import KiliAuth
-from ...helpers import Compatible, check_file_is_py, format_result
-from .queries import (
-    GQL_ACTIVATE_PLUGIN_ON_PROJECT,
-    GQL_DEACTIVATE_PLUGIN_ON_PROJECT,
-    GQL_UPLOAD_PLUGIN_BETA,
-)
+from ...helpers import Compatible, format_result
+from .queries import GQL_ACTIVATE_PLUGIN_ON_PROJECT, GQL_DEACTIVATE_PLUGIN_ON_PROJECT
 
 
 class MutationsPlugins:
@@ -51,28 +49,8 @@ class MutationsPlugins:
             >>> kili.upload_plugin_beta(file_path="./path/to/my/file.py")
         """
 
-        if not check_file_is_py(file_path, verbose):
-            raise ValueError("Wrong file format. ")
-
-        path = Path(file_path)
-
-        with path.open("r", encoding="utf-8") as file:
-            source_code = file.read()
-
-        # We execute the source code to prevent the upload of a file with SyntaxError
-        print(f"Executing {path.name}...")
-        # pylint: disable=exec-used
-        exec(source_code)
-        print(f"Done executing {path.name}!")
-
-        if plugin_name is None:
-            plugin_name = path.name
-
-        variables = {"pluginName": plugin_name, "pluginSrc": source_code}
-        result = self.auth.client.execute(GQL_UPLOAD_PLUGIN_BETA, variables)
-
-        pretty_result = format_result("data", result)
-        return pretty_result
+        result = PluginUploader(self.auth, file_path, plugin_name, verbose).create_plugin()
+        return result
 
     @Compatible(endpoints=["v2"])
     @typechecked
