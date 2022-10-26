5ceee19bea69f33a3244249c655a514145304b84
Pierre Leveau <PierreLeveau@users.noreply.github.com>
2022-10-26 12:39:33 +0200

    fix: obfuscate API key when auth failure (#728)

--- tests/test_client.py
+++ tests/test_client.py
@@ -19,3 +19,17 @@ class TestClient:
     def test_no_api_key(self, monkeypatch):
         with pytest.raises(AuthenticationFailed):
             _ = Kili()
+
+    @mock.patch.dict(os.environ, {}, clear=True)
+    def test_wrong_api_key(self, monkeypatch):
+        with pytest.raises(AuthenticationFailed) as e_info:
+            _ = Kili(api_key="wrong_api_key")
+
+        assert "failed with API key: *********_key" in str(e_info)
+
+    @mock.patch.dict(os.environ, {}, clear=True)
+    def test_wrong_api_key_shot(self, monkeypatch):
+        with pytest.raises(AuthenticationFailed) as e_info:
+            _ = Kili(api_key="no")
+
+        assert "failed with API key: no" in str(e_info)
