8e7b5bad53fe528e396f6814479ecd208274a677
Antonio Loison <antonio.loison@kili-technology.com>
2022-10-27 11:35:07 +0200

    feat: make import from single raw format file

--- src/kili/services/label_import/importer/__init__.py
+++ src/kili/services/label_import/importer/__init__.py
@@ -2,6 +2,7 @@
 Label Importers
 """
 import csv
+import json
 import logging
 from abc import ABC, abstractmethod
 from pathlib import Path
@@ -71,29 +72,28 @@ class AbstractLabelImporter(ABC):
         label_parser = label_parser_class(class_by_id, target_job_name)
 
         self.logger.warning("Importing labels")
-        labels = self.extract_from_files(labels_file_path, labels_files)
+        labels = self.extract_from_files(labels_file_path, labels_files, label_parser)
         if is_prediction:
             assert model_name
-            self._import_as_predictions(labels, label_parser, project_id, model_name)
+            self._import_as_predictions(labels, project_id, model_name)
         else:
-            self._import_as_labels(labels, label_parser, project_id)
+            self._import_as_labels(labels, project_id)
 
         self.logger.warning(print(f"{len(labels)} labels have been successfully imported"))
 
-    def _import_as_labels(
-        self, labels: List[LabelToImport], label_parser: AbstractLabelParser, project_id: ProjectId
-    ):
+    def _import_as_labels(self, labels: List[LabelToImport], project_id: ProjectId):
         for label in tqdm(labels, disable=self.logger_params.disable_tqdm):
-            kwargs = dict(label.copy())
-            del kwargs["path"]
-            try:
-                json_response = label_parser.parse(Path(label["path"]))
-            except Exception as exc:
-                path = label["path"]
-                raise LabelParsingError(f"Failed to parse the file {path}") from exc
-
+            # print(label.keys())
+            # print(label["latestLabel"].keys())
+            kwargs = {
+                "author_id": label["latestLabel"]["author"]["id"],
+                "label_asset_external_id": label["externalId"],
+                "label_asset_id": label["id"],
+                "label_type": label["latestLabel"]["labelType"],
+                "seconds_to_label": label.get("seconds_to_label", None),
+            }
             self.kili.append_to_labels(
-                json_response=json_response,
+                json_response=label,
                 project_id=project_id,
                 **kwargs,
             )
@@ -101,7 +101,6 @@ class AbstractLabelImporter(ABC):
     def _import_as_predictions(
         self,
         labels: List[LabelToImport],
-        label_parser: AbstractLabelParser,
         project_id: ProjectId,
         model_name: str,
     ):
@@ -110,13 +109,8 @@ class AbstractLabelImporter(ABC):
         external_id_array: List[str] = []
         model_name_array: List[str] = []
         for label in tqdm(labels, disable=self.logger_params.disable_tqdm):
-            kwargs = dict(label.copy())
-            del kwargs["path"]
-            json_response_array.append(label_parser.parse(Path(label["path"])))
+            json_response_array.append(label)
             external_id = label.get("label_asset_external_id")
-            if external_id is None:
-                path = label["path"]
-                raise MissingExternalIdError(f"There is no associated asset external id to {path}")
             external_id_array.append(external_id)
             model_name_array.append(model_name)
         self.kili.create_predictions(
@@ -148,9 +142,27 @@ class AbstractLabelImporter(ABC):
     def _get_label_file_extension(self) -> str:
         pass
 
+    @staticmethod
+    def parse_label(label, label_parser) -> Dict[str, Any]:
+        kwargs = dict(label.copy())
+        del kwargs["path"]
+        try:
+            json_response = label_parser.parse(Path(label["path"]))
+        except Exception as exc:
+            path = label["path"]
+            raise LabelParsingError(f"Failed to parse the file {path}") from exc
+        external_id = label.get("label_asset_external_id")
+        if external_id is None:
+            path = label["path"]
+            raise MissingExternalIdError(f"There is no associated asset external id to {path}")
+        return json_response
+
     def extract_from_files(
-        self, labels_file_path: Optional[Path], labels_files: Optional[List[Path]]
-    ) -> List[LabelToImport]:
+        self,
+        labels_file_path: Optional[Path],
+        labels_files: Optional[List[Path]],
+        label_parser: AbstractLabelParser,
+    ) -> List[Dict[str, Any]]:
         """
         Extracts the labels files and their metadata from the label files (given
         explicitely or through a CSV)
@@ -159,31 +171,75 @@ class AbstractLabelImporter(ABC):
 
         labels = []
         if labels_files is not None and len(labels_files) > 0:
-            label_paths = get_file_paths_to_upload(
-                [str(f) for f in labels_files],
-                lambda path: path.endswith(self._get_label_file_extension()),
-                verbose=self.logger_params.level in ["INFO", "DEBUG"],
-            )
-            if len(label_paths) == 0:
-                raise ValueError(
-                    "No label files to upload. "
-                    "Check that the paths exist and file types are .json"
+            if len(labels_files) == 1 and labels_files[0].suffix == ".json":
+                labels = self._read_labels_file_path_from_json(labels_files[0])
+            else:
+                print("hello")
+                label_paths = get_file_paths_to_upload(
+                    [str(f) for f in labels_files],
+                    lambda path: path.endswith(self._get_label_file_extension()),
+                    verbose=self.logger_params.level in ["INFO", "DEBUG"],
                 )
-            external_ids = [get_external_id_from_file_path(Path(path)) for path in label_paths]
-            labels = [
-                LabelToImport(path=p, label_asset_external_id=e_id)
-                for (p, e_id) in zip(label_paths, external_ids)
-            ]
+                if len(label_paths) == 0:
+                    raise ValueError(
+                        "No label files to upload. "
+                        "Check that the paths exist and file types are .json"
+                    )
+                external_ids = [get_external_id_from_file_path(Path(path)) for path in label_paths]
+                labels = [
+                    self.parse_label(LabelToImport(path=p, label_asset_external_id=e_id), label_parser)
+                    for (p, e_id) in zip(label_paths, external_ids)
+                ]
 
         elif labels_file_path is not None:
-            labels = self._read_labels_file_path(labels_file_path)
+            labels = self._read_labels_file_path_from_csv(labels_file_path)
 
             if len(labels) == 0:
                 raise ValueError(f"No label file was found in csv: {labels_file_path}")
+            labels = [self.parse_label(label, label_parser) for label in labels]
         return labels
 
     @classmethod
-    def _read_labels_file_path(cls, labels_file: Path) -> List[LabelToImport]:
+    def _read_labels_file_path_from_json(cls, labels_file: Path) -> List[Dict[str, Any]]:
+        """
+        Read CSV and fill a list of labels to import.
+        """
+        with labels_file.open("r") as f:
+            return json.load(f)
+        # with labels_file.open("r") as l_f:
+        #     csv_reader = csv.reader(l_f)
+        #     data = []
+        #     headers = []
+        #     for ind, row in enumerate(csv_reader):
+        #         if ind == 0:
+        #             headers = row
+        #         else:
+        #             headers_row = zip(headers, row)
+        #             str_dict = dict(headers_row)
+
+        #             typed_dict: Dict[str, Any] = {
+        #                 "path": str_dict["path"],
+        #             }
+        #             if len(str_dict.get("label_asset_id", "")):
+        #                 typed_dict["label_asset_id"] = str_dict["label_asset_id"]
+
+        #             if len(str_dict.get("label_asset_external_id", "")):
+        #                 typed_dict["label_asset_external_id"] = str_dict["label_asset_external_id"]
+
+        #             if len(str_dict.get("label_type", "")):
+        #                 typed_dict["label_type"] = cast(LabelType, str_dict["label_type"])
+
+        #             if len(str_dict.get("seconds_to_label", "")):
+        #                 typed_dict["seconds_to_label"] = int(str_dict["seconds_to_label"])
+
+        #             if len(str_dict.get("author_id", "")):
+        #                 typed_dict["author_id"] = str_dict["author_id"]
+
+        #             data.append(LabelToImport(**typed_dict))
+        # return data
+
+    @classmethod
+    def _read_labels_file_path_from_csv(cls, labels_file: Path) -> List[LabelToImport]:
         """
         Read CSV and fill a list of labels to import.
         """
