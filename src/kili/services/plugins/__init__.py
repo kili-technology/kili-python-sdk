facba9793d562ad95f6822d97f5403f8fb8a32bd
pmKili <77961716+pmKili@users.noreply.github.com>
2022-11-14 15:53:43 +0100

    Feature/data 810 aau on plugins manager i see i can (#764)

--- src/kili/services/plugins/__init__.py
+++ src/kili/services/plugins/__init__.py
@@ -2,7 +2,15 @@
 
 from kili.services.plugins.activation import activate_plugin, deactivate_plugin
 from kili.services.plugins.deletion import delete_plugin
+from kili.services.plugins.list import list_plugins
 from kili.services.plugins.logs import get_logs
 from kili.services.plugins.upload import PluginUploader
 
-__all__ = ["PluginUploader", "activate_plugin", "deactivate_plugin", "delete_plugin", "get_logs"]
+__all__ = [
+    "PluginUploader",
+    "activate_plugin",
+    "deactivate_plugin",
+    "delete_plugin",
+    "get_logs",
+    "list_plugins",
+]
