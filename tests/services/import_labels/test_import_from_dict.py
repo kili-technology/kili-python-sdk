bfdadf5b769e0d6ecd352058c3be2391e7055a79
Theo Dullin <theo@kili-technology.com>
2022-12-19 12:48:57 +0100

    chore: add test for import_labels_from_dict

---
+++ tests/services/import_labels/test_import_from_dict.py
@@ -0,0 +1,192 @@
+import json
+from unittest.mock import MagicMock
+
+import pydantic
+import pytest
+
+from kili.graphql.operations.label.mutations import GQL_APPEND_MANY_LABELS
+from kili.services.label_import import import_labels_from_dict
+
+
+class TestImportLabelsFromDict:
+    """Test class for import_label_from_dict_service"""
+
+    def setup_class(self):
+        self.kili = MagicMock()
+        self.kili.auth.client.execute = MagicMock()
+        with open("./tests/services/import_labels/fixtures/json_response_image.json") as json_file:
+            self.json_response = json.load(json_file)
+
+    def test_import_default_labels_with_asset_id(self):
+        project_id = "project_id"
+        label_type = "DEFAULT"
+        model_name = None
+        labels = [
+            {"json_response": self.json_response, "asset_id": "asset_id_1"},
+            {"json_response": self.json_response, "asset_id": "asset_id_2"},
+        ]
+
+        call = {
+            "data": {
+                "labelType": "DEFAULT",
+                "labelsData": [
+                    {
+                        "assetID": "asset_id_1",
+                        "authorID": None,
+                        "jsonResponse": json.dumps(self.json_response),
+                        "modelName": None,
+                        "secondsToLabel": None,
+                    },
+                    {
+                        "assetID": "asset_id_2",
+                        "authorID": None,
+                        "jsonResponse": json.dumps(self.json_response),
+                        "modelName": None,
+                        "secondsToLabel": None,
+                    },
+                ],
+            },
+            "where": {"idIn": ["asset_id_1", "asset_id_2"]},
+        }
+
+        import_labels_from_dict(self.kili, project_id, labels, label_type, model_name)
+        self.kili.auth.client.execute.assert_called_with(GQL_APPEND_MANY_LABELS, call)
+
+    def test_import_default_labels_with_external_id(self):
+        self.kili.assets = MagicMock(
+            return_value=[
+                {"id": "asset_id_1", "externalId": "asset_external_id_1"},
+                {"id": "asset_id_2", "externalId": "asset_external_id_2"},
+            ]
+        )
+        project_id = "project_id"
+        label_type = "DEFAULT"
+        model_name = None
+        labels = [
+            {"json_response": self.json_response, "asset_external_id": "asset_external_id_1"},
+            {"json_response": self.json_response, "asset_external_id": "asset_external_id_2"},
+        ]
+
+        call = {
+            "data": {
+                "labelType": "DEFAULT",
+                "labelsData": [
+                    {
+                        "assetID": "asset_id_1",
+                        "authorID": None,
+                        "jsonResponse": json.dumps(self.json_response),
+                        "modelName": None,
+                        "secondsToLabel": None,
+                    },
+                    {
+                        "assetID": "asset_id_2",
+                        "authorID": None,
+                        "jsonResponse": json.dumps(self.json_response),
+                        "modelName": None,
+                        "secondsToLabel": None,
+                    },
+                ],
+            },
+            "where": {"idIn": ["asset_id_1", "asset_id_2"]},
+        }
+
+        import_labels_from_dict(self.kili, project_id, labels, label_type, model_name)
+        self.kili.auth.client.execute.assert_called_with(GQL_APPEND_MANY_LABELS, call)
+
+    def test_import_labels_with_optional_params(self):
+        project_id = "project_id"
+        label_type = "DEFAULT"
+        model_name = None
+        author_id = "author_id"
+        seconds_to_label = 3
+        labels = [
+            {
+                "json_response": self.json_response,
+                "asset_id": "asset_id",
+                "author_id": author_id,
+                "seconds_to_label": seconds_to_label,
+            }
+        ]
+
+        call = {
+            "data": {
+                "labelType": "DEFAULT",
+                "labelsData": [
+                    {
+                        "assetID": "asset_id",
+                        "authorID": author_id,
+                        "jsonResponse": json.dumps(self.json_response),
+                        "modelName": None,
+                        "secondsToLabel": seconds_to_label,
+                    }
+                ],
+            },
+            "where": {"idIn": ["asset_id"]},
+        }
+
+        import_labels_from_dict(self.kili, project_id, labels, label_type, model_name)
+        self.kili.auth.client.execute.assert_called_with(GQL_APPEND_MANY_LABELS, call)
+
+    def test_return_error_when_give_unexisting_label_field(self):
+        project_id = "project_id"
+        label_type = "DEFAULT"
+        model_name = None
+        labels = [
+            {"json_response": self.json_response, "asset_id": "asset_id", "unexisting_field": 3}
+        ]
+        with pytest.raises(pydantic.ValidationError):
+            import_labels_from_dict(self.kili, project_id, labels, label_type, model_name)
+
+    def test_return_error_when_give_wrong_field_type(self):
+        project_id = "project_id"
+        label_type = "DEFAULT"
+        model_name = None
+        labels = [
+            {
+                "json_response": self.json_response,
+                "asset_id": "asset_id",
+                "seconds_to_label": "wrong_type",
+            }
+        ]
+        with pytest.raises(pydantic.ValidationError):
+            import_labels_from_dict(self.kili, project_id, labels, label_type, model_name)
+
+    def test_import_predictions(self):
+        self.kili.assets = MagicMock(
+            return_value=[{"id": "asset_id", "externalId": "asset_external_id"}]
+        )
+        project_id = "project_id"
+        label_type = "PREDICTION"
+        model_name = "model_name"
+        labels = [{"json_response": self.json_response, "asset_external_id": "asset_external_id"}]
+
+        call = {
+            "data": {
+                "labelType": "PREDICTION",
+                "labelsData": [
+                    {
+                        "assetID": "asset_id",
+                        "authorID": None,
+                        "jsonResponse": json.dumps(self.json_response),
+                        "modelName": model_name,
+                        "secondsToLabel": None,
+                    }
+                ],
+            },
+            "where": {"idIn": ["asset_id"]},
+        }
+
+        import_labels_from_dict(self.kili, project_id, labels, label_type, model_name)
+        self.kili.auth.client.execute.assert_called_with(GQL_APPEND_MANY_LABELS, call)
+
+    def test_import_predictions_without_giving_model_name(self):
+        self.kili.assets = MagicMock(
+            return_value=[{"id": "asset_id", "externalId": "asset_external_id"}]
+        )
+        project_id = "project_id"
+        label_type = "PREDICTION"
+        model_name = None
+        labels = [{"json_response": self.json_response, "asset_external_id": "asset_external_id"}]
+
+        with pytest.raises(ValueError):
+            import_labels_from_dict(self.kili, project_id, labels, label_type, model_name)
