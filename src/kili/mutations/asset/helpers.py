9138a348d8cb7f56c575b941e4b9af2704cf054a
Jonas Maison <Jonas1312@users.noreply.github.com>
2023-01-04 10:18:09 +0100

    feat: add external ids parameter to methods

--- src/kili/mutations/asset/helpers.py
+++ src/kili/mutations/asset/helpers.py
@@ -47,9 +47,10 @@ def get_asset_ids_or_throw_error(
     """
     check_asset_identifier_arguments(project_id, asset_ids, external_ids)
 
-    id_map = infer_ids_from_external_ids(
-        kili=kili, asset_external_ids=external_ids, project_id=project_id  # type: ignore
-    )
-    asset_ids = [id_map[id] for id in external_ids]  # type: ignore
+    if asset_ids is None:
+        id_map = infer_ids_from_external_ids(
+            kili=kili, asset_external_ids=external_ids, project_id=project_id  # type: ignore
+        )
+        asset_ids = [id_map[id] for id in external_ids]  # type: ignore
 
-    return asset_ids  # type: ignore
+    return asset_ids
