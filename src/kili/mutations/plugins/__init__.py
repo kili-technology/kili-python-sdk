b9707b2f95d9b9d34d0e911c9de364dcb54fd57b
pmKili <77961716+pmKili@users.noreply.github.com>
2023-01-12 15:05:41 +0100

    feat(DATA-892): upload directory as plugin_src (#880)

--- src/kili/mutations/plugins/__init__.py
+++ src/kili/mutations/plugins/__init__.py
@@ -28,15 +28,16 @@ class MutationsPlugins:
     @typechecked
     def upload_plugin(
         self,
-        file_path: str,
+        plugin_path: Optional[str] = None,
         plugin_name: Optional[str] = None,
         verbose: bool = True,
+        **kwargs
     ):
         # pylint: disable=line-too-long
         """Uploads a plugin.
 
         Args:
-            file_path : Path to your .py file
+            plugin_path : Path to your plugin. Either a folder containing a main.py (mandatory) and a requirements.txt (optional) or a .py file
             plugin_name: name of your plugin, if not provided, it will be the name from your file
             verbose: If false, minimal logs are displayed
 
@@ -45,10 +46,18 @@ class MutationsPlugins:
                 or an error message.
 
         Examples:
-            >>> kili.upload_plugin(file_path="./path/to/my/file.py")
+            >>> kili.upload_plugin(plugin_path="./path/to/my/file.py")
         """
 
-        return PluginUploader(self.auth, file_path, plugin_name, verbose).create_plugin()
+        if kwargs.get("file_path"):
+            raise TypeError(
+                '"file_path" has been deprecated for "plugin_path", please use "plugin_path" instead'
+            )
+
+        if not plugin_path:
+            raise TypeError('"plugin_path is nullish, please provide a value')
+
+        return PluginUploader(self.auth, plugin_path, plugin_name, verbose).create_plugin()
 
     @typechecked
     def activate_plugin_on_project(
@@ -123,14 +132,17 @@ class MutationsPlugins:
     @typechecked
     def update_plugin(
         self,
-        file_path: str,
-        plugin_name: str,
+        plugin_path: Optional[str] = None,
+        plugin_name: Optional[str] = None,
         verbose: bool = True,
+        **kwargs
     ):
         """Update a plugin with new code.
 
         Args:
-            file_path : Path to your .py file
+            plugin_path : Path to your plugin. Either:
+             - a folder containing a main.py (mandatory) and a requirements.txt (optional)
+             - a .py file
             plugin_name: Name of the plugin
             verbose: If false, minimal logs are displayed
 
@@ -142,4 +154,16 @@ class MutationsPlugins:
             >>> kili.update_plugin(plugin_name="my_plugin_name")
         """
 
-        return PluginUploader(self.auth, file_path, plugin_name, verbose).update_plugin()
+        if kwargs.get("file_path"):
+            raise TypeError(
+                """ "file_path" has been deprecated for "plugin_path",
+                please use "plugin_path" instead"""
+            )
+
+        if not plugin_path:
+            raise TypeError('"plugin_path is nullish, please provide a value')
+
+        if not plugin_name:
+            raise TypeError('"plugin_name is nullish, please provide a value')
+
+        return PluginUploader(self.auth, plugin_path, plugin_name, verbose).update_plugin()
--- src/kili/services/plugins/upload.py
+++ src/kili/services/plugins/upload.py
@@ -4,10 +4,11 @@ Class to upload a plugin
 
 import time
 from pathlib import Path
-from typing import Optional
+from typing import List, Optional, Union
+from zipfile import ZipFile
 
 from kili.authentication import KiliAuth
-from kili.constants import mime_extensions_for_py_scripts
+from kili.constants import mime_extensions_for_py_scripts, mime_extensions_for_txt_files
 from kili.graphql.operations.plugins.mutations import (
     GQL_CREATE_PLUGIN,
     GQL_CREATE_PLUGIN_RUNNER,
@@ -18,78 +19,132 @@ from kili.graphql.operations.plugins.queries import GQL_GET_PLUGIN_RUNNER_STATUS
 from kili.helpers import format_result, get_data_type
 from kili.services.plugins.tools import check_errors_plugin_upload
 from kili.utils import bucket
+from kili.utils.tempfile import TemporaryDirectory
 
 from .helpers import get_logger
 
 NUMBER_TRIES_RUNNER_STATUS = 20
 
 
-def check_file_is_py(path: Path, verbose: bool = True) -> bool:
+def check_file_mime_type(
+    path: Path, compatible_mime_extensions: List[str], verbose: bool = True
+) -> bool:
     """
-    Returns true if the mime type of the file corresponds to a python file
+    Returns true if the mime type of the file corresponds to one of compatible_mime_extensions
     """
     mime_type = get_data_type(path.as_posix())
 
-    if not (mime_extensions_for_py_scripts and mime_type):
+    if not (compatible_mime_extensions and mime_type):
         return False
 
-    correct_mime_type = mime_type in mime_extensions_for_py_scripts
+    correct_mime_type = mime_type in compatible_mime_extensions
 
     if verbose and not correct_mime_type:
         print(
             f"File mime type for {path} is {mime_type} and does not correspond"
             "to the type of the project. "
-            f"File mime type should be one of {mime_extensions_for_py_scripts}"
+            f"File mime type should be one of {compatible_mime_extensions}"
         )
     return correct_mime_type
 
 
-class PluginUploader:  # pylint: disable=too-few-public-methods
+def check_file_is_py(path: Path, verbose: bool = True) -> bool:
+    """
+    Returns true if the mime type of the file corresponds to a python file
+    """
+    return check_file_mime_type(path, mime_extensions_for_py_scripts, verbose)
+
+
+def check_file_is_txt(path: Path, verbose: bool = True) -> bool:
+    """
+    Returns true if the mime type of the file corresponds to a .txt file
+    """
+    return check_file_mime_type(path, mime_extensions_for_txt_files, verbose)
+
+
+class PluginUploader:
     """
     Class to upload a plugin
     """
 
     def __init__(
-        self, auth: KiliAuth, file_path: str, plugin_name: Optional[str], verbose: bool
+        self, auth: KiliAuth, plugin_path: str, plugin_name: Optional[str], verbose: bool
     ) -> None:
         self.auth = auth
-        self.file_path = Path(file_path)
-        self.plugin_name = plugin_name or self.file_path.name
+        self.plugin_path = Path(plugin_path)
+
+        if (not self.plugin_path.is_dir()) and (not self.plugin_path.is_file()):
+            raise FileNotFoundError(
+                f"""The provided path "{plugin_path}" is neither a directory nor a file.
+                The absolute path is the following: '{self.plugin_path.absolute()}'"""
+            )
+
+        if plugin_name:
+            self.plugin_name = plugin_name
+        else:
+            self.plugin_name = self.plugin_path.name
         self.verbose = verbose
 
-    def _retrieve_script(self) -> str:
+    def _retrieve_script(self) -> Path:
+        """
+        Retrieve script from plugin_path and execute it
+        to prevent an upload with indentation errors
+        """
+
+        if self.plugin_path.is_dir():
+            file_path = self.plugin_path / "main.py"
+            if not file_path.is_file():
+                raise FileNotFoundError(
+                    f"No main.py file in the provided folder: {self.plugin_path.absolute()}"
+                )
+        else:
+            file_path = self.plugin_path
+
+        if not check_file_is_py(file_path, self.verbose):
+            raise ValueError("Wrong file format.")
+
+        return file_path
+
+    def _retrieve_requirements(self) -> Union[Path, None]:
         """
         Retrieve script from file_path and execute it
         to prevent an upload with indentation errors
         """
-        if not check_file_is_py(self.file_path, self.verbose):
-            raise ValueError("Wrong file format. ")
 
-        path = Path(self.file_path)
+        if not self.plugin_path.is_dir():
+            return None
 
-        with path.open("r", encoding="utf-8") as file:
-            source_code = file.read()
+        file_path = self.plugin_path / "requirements.txt"
+        if not file_path.is_file():
+            logger = get_logger()
+            logger.info(
+                f"No requirements.txt file in the provided folder: {self.plugin_path.absolute()}"
+            )
+            return None
 
-        return source_code
+        if not check_file_is_txt(file_path, self.verbose):
+            raise ValueError("Wrong file format.")
 
-    def _parse_script(self, source_code: str):
+        return file_path
+
+    @staticmethod
+    def _parse_script(script_path: Path):
         """
         Method to detect indentation errors in the script
         """
-        logger = get_logger()
+        with script_path.open("r", encoding="utf-8") as file:
+            source_code = file.read()
 
         # We execute the source code to prevent the upload of a file with SyntaxError
-        logger.info(f"Executing {self.file_path.name}...")
         # pylint: disable=exec-used
         exec(source_code, globals())
-        logger.info(f"Done executing {self.file_path.name}!")
 
     @staticmethod
-    def _upload_file(source_code: str, url: str):
+    def _upload_file(zip_path: Path, url: str):
         """
         Upload a file to a signed url and returns the url with the file_id
         """
-        bucket.upload_data_via_rest(url, source_code.encode("utf-8"), "text/x-python")
+        bucket.upload_data_via_rest(url, zip_path.read_bytes(), "application/zip")
 
     def _retrieve_upload_url(self, is_updating_plugin: bool) -> str:
         """
@@ -102,9 +157,24 @@ class PluginUploader:  # pylint: disable=too-few-public-methods
         else:
             result = self.auth.client.execute(GQL_CREATE_PLUGIN, variables)
 
-        check_errors_plugin_upload(result, self.file_path, self.plugin_name)
+        check_errors_plugin_upload(result, self.plugin_path, self.plugin_name)
         upload_url = format_result("data", result)
         return upload_url
+
+    @staticmethod
+    def _create_zip(script_path: Path, requirements_path: Optional[Path], tmp_directory: Path):
+        """
+        Create a zip file from python file and requirements.txt
+        (if user has defined a path to a requirements.txt)
+        """
+        zip_path = tmp_directory / "archive.zip"
+
+        with ZipFile(zip_path, "w") as archive:
+            archive.write(script_path, "main.py")
+            if requirements_path:
+                archive.write(requirements_path)
+
+        return zip_path
 
     def _upload_script(self, is_updating_plugin: bool):
         """
@@ -113,9 +183,16 @@ class PluginUploader:  # pylint: disable=too-few-public-methods
 
         upload_url = self._retrieve_upload_url(is_updating_plugin)
 
-        source_code = self._retrieve_script()
-        self._parse_script(source_code)
-        self._upload_file(source_code, upload_url)
+        script_path = self._retrieve_script()
+        self._parse_script(script_path)
+
+        requirements = self._retrieve_requirements()
+
+        with TemporaryDirectory() as tmp_directory:
+
+            zip_path = self._create_zip(script_path, requirements, tmp_directory)
+
+            self._upload_file(zip_path, upload_url)
 
     def _create_plugin_runner(self):
         """
