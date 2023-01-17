cd25ef1e0a39352b899a4ddabc0a19b73c1aa9a6
p-desaintchamas <49166437+p-desaintchamas@users.noreply.github.com>
2023-01-17 11:29:07 +0100

    feat: add webhook creation & update (#900)

--- src/kili/services/plugins/__init__.py
+++ src/kili/services/plugins/__init__.py
@@ -4,10 +4,11 @@ from kili.services.plugins.activation import activate_plugin, deactivate_plugin
 from kili.services.plugins.deletion import delete_plugin
 from kili.services.plugins.list import list_plugins
 from kili.services.plugins.logs import get_logs
-from kili.services.plugins.upload import PluginUploader
+from kili.services.plugins.upload import PluginUploader, WebhookUploader
 
 __all__ = [
     "PluginUploader",
+    "WebhookUploader",
     "activate_plugin",
     "deactivate_plugin",
     "delete_plugin",
