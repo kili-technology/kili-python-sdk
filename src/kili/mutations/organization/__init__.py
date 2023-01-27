f4fc184e5cd0bcf29bbd7d89c7970e151fc0624e
Jonas Maison <Jonas1312@users.noreply.github.com>
2023-01-27 11:30:48 +0100

    Update src/kili/mutations/organization/__init__.py
    Co-authored-by: Pierre Leveau <PierreLeveau@users.noreply.github.com>

--- src/kili/mutations/organization/__init__.py
+++ src/kili/mutations/organization/__init__.py
@@ -25,7 +25,7 @@ class MutationsOrganization:
     @typechecked
     def create_organization(self, name: str, address: str, zip_code: str, city: str, country: str):
         """Create an organization.
-        WARNING: This method is for internal use by Kili Technology only.
+        WARNING: This method is for internal use only.
 
         Each user must be linked to an organization
 
