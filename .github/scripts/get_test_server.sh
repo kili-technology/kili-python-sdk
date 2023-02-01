6fb5060201431da087b9fb694628f6d00c045c43
Jonas Maison <Jonas1312@users.noreply.github.com>
2023-02-01 11:07:30 +0100

    ci: run tests against proper server

--- .github/scripts/get_test_server.sh
+++ .github/scripts/get_test_server.sh
@@ -1,36 +1,31 @@
 #!/bin/bash
 
 # Script that determines the test server to run the tests against.
+# The test server is echoed in stdout
+# To get test server, parse: TEST_AGAINST=<value>;
+# To get endpoint, parse: KILI_API_ENDPOINT=<value>;
 
 current_branch=$(git rev-parse --abbrev-ref HEAD)
 echo "current_branch: $current_branch"
 
 if [[ $current_branch = release/* ]]; then
+    # we are on a release branch, test against preprod or prod
     sdk_version=$(cat setup.cfg | grep current_version | cut -d ' ' -f 3)
     echo "sdk_version: $sdk_version"
-    IFS=. read -r major minor patch <<< "$sdk_version"
-    sdk_version_major_minor="$major.$minor"
+    IFS=. read -r sdk_major sdk_minor sdk_patch <<< "$sdk_version"
 
-    prod_version=$(curl --silent --max-time 5 "https://cloud.kili-technology.com/api/label/v2/version" | jq -r .version)
-    echo "prod_version: $prod_version"
-    IFS=. read -r major minor patch <<< "$prod_version"
-    prod_version_major_minor="$major.$minor"
-
-    preprod_version=$(curl --silent --max-time 5 "https://preproduction.cloud.kili-technology.com/api/label/v2/version" | jq -r .version)
-    echo "preprod_version: $preprod_version"
-    IFS=. read -r major minor patch <<< "$preprod_version"
-    preprod_version_major_minor="$major.$minor"
-
-    if [[ $sdk_version_major_minor = $preprod_version_major_minor ]]; then
+    if [[ "$sdk_patch" == "0" ]]; then
+        # minor release, we test against preprod
         echo "TEST_AGAINST=PREPROD;"
         echo "KILI_API_ENDPOINT=https://preproduction.cloud.kili-technology.com/api/label/v2/graphql;"
-    elif [[ $sdk_version_major_minor = $prod_version_major_minor ]]; then
+    else
+        # patch release, we test against prod
         echo "TEST_AGAINST=PROD;"
         echo "KILI_API_ENDPOINT=https://cloud.kili-technology.com/api/label/v2/graphql;"
-    else
-        echo "ERROR: SDK version $sdk_version_major_minor does not match neither preprod version $preprod_version nor prod version $prod_version"
     fi
+
 else
+    # master or dev branch, we test against staging
     echo "TEST_AGAINST=STAGING;"
     echo "KILI_API_ENDPOINT=https://staging.cloud.kili-technology.com/api/label/v2/graphql;"
 fi
