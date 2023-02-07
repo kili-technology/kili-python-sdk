8aee99d36a6278ec6637aa54ac32ccb6c9f65662
Jonas Maison <Jonas1312@users.noreply.github.com>
2023-02-07 16:46:26 +0100

    ci: update and fix release CIs (#976)

--- tests/test_client.py
+++ tests/test_client.py
@@ -12,14 +12,14 @@ from kili.exceptions import AuthenticationFailed
 
 def test_no_api_key():
     with patch.dict(os.environ):
-        os.environ.pop("KILI_API_KEY")
+        os.environ.pop("KILI_API_KEY", None)
         with pytest.raises(AuthenticationFailed):
             _ = Kili()
 
 
 def test_wrong_api_key():
     with patch.dict(os.environ):
-        os.environ.pop("KILI_API_KEY")
+        os.environ.pop("KILI_API_KEY", None)
         with pytest.raises(AuthenticationFailed) as e_info:
             _ = Kili(api_key="wrong_api_key")
 
@@ -28,7 +28,7 @@ def test_wrong_api_key():
 
 def test_wrong_api_key_shot():
     with patch.dict(os.environ):
-        os.environ.pop("KILI_API_KEY")
+        os.environ.pop("KILI_API_KEY", None)
         with pytest.raises(AuthenticationFailed) as e_info:
             _ = Kili(api_key="no")
 
