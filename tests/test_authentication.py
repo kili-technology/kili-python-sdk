de8f299554153bbb301d58bb1ddbaeb22d398f08
Jonas Maison <Jonas1312@users.noreply.github.com>
2023-02-12 21:32:21 +0100

    refactor: change GraphQL client for gql

--- tests/test_authentication.py
+++ tests/test_authentication.py
@@ -11,7 +11,7 @@ from kili.graphql.graphql_client import GraphQLClientName
 @patch("kili.authentication.requests")
 @patch.object(KiliAuth, "check_versions_match", side_effect=Exception)
 def test_warn_cant_check_kili_version(mocked_requests, mocked_check_versions_match):
-    with pytest.raises(Exception):
+    with pytest.raises(Exception):  # TODO: fix that
         with pytest.warns(
             UserWarning, match="We could not check the version, there might be a version"
         ):
