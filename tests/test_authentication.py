1e08f573c37cb4d65b9becd9878e5404f4a40035
Jonas Maison <Jonas1312@users.noreply.github.com>
2023-02-12 18:29:33 +0100

    refactor: change GraphQL client for gql

--- tests/test_authentication.py
+++ tests/test_authentication.py
@@ -1,3 +1,5 @@
+import datetime
+import warnings
 from unittest.mock import patch
 
 import pytest
@@ -13,4 +15,20 @@ def test_warn_cant_check_kili_version(mocked_requests, mocked_check_versions_mat
         with pytest.warns(
             UserWarning, match="We could not check the version, there might be a version"
         ):
-            auth = KiliAuth(api_key="", api_endpoint="", client_name=GraphQLClientName.SDK)
+            _ = KiliAuth(api_key="", api_endpoint="", client_name=GraphQLClientName.SDK)
+
+
+@patch("kili.authentication.requests")
+@patch.object(KiliAuth, "check_versions_match", return_value=True)
+@patch.object(KiliAuth, "check_api_key_valid", return_value=True)
+@patch("kili.authentication.GraphQLClient", return_value=None)
+@patch("kili.authentication.APIKeyQuery.__call__")
+def test_check_expiry_of_key_is_close(mocker_api_key_query, *_):
+    mocker_api_key_query.return_value = yield {"createdAt": "2020-01-01T00:00:00.000Z"}
+    with pytest.warns(UserWarning, match="Your api key will be deprecated on"):
+        _ = KiliAuth(api_key="", api_endpoint="", client_name=GraphQLClientName.SDK)
+
+    mocker_api_key_query.return_value = yield {"createdAt": datetime.datetime.now().isoformat()}
+    with warnings.catch_warnings():
+        warnings.simplefilter("error")
+        _ = KiliAuth(api_key="", api_endpoint="", client_name=GraphQLClientName.SDK)
