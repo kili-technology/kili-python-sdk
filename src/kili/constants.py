3bcc0d722790a588219574f78e90663bbb8f3b09
Jonas Maison <Jonas1312@users.noreply.github.com>
2023-03-02 17:00:21 +0100

    feat: add `kili.synchronize_cloud_storage_connection` (#1035)
    Co-authored-by: theodu <82399630+theodu@users.noreply.github.com>

--- src/kili/constants.py
+++ src/kili/constants.py
@@ -49,5 +49,6 @@ mime_extensions_for_IV2 = {
 mime_extensions_for_py_scripts = ["text/x-python"]
 mime_extensions_for_txt_files = ["text/plain"]
 
+QUERY_BATCH_SIZE = 100
 MUTATION_BATCH_SIZE = 100
 THROTTLING_DELAY = 60 / 250
