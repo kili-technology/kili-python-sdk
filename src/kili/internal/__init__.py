242437c0b9b6d70105e479e3c9542a157878670d
theodu <82399630+theodu@users.noreply.github.com>
2023-03-09 17:14:46 +0100

    feat: move internal_delete_project in internal folder (#1081)

--- src/kili/internal/__init__.py
+++ src/kili/internal/__init__.py
@@ -5,6 +5,7 @@ Module for methods and classes that are for internal use by Kili Technology only
 from typeguard import typechecked
 
 from kili.helpers import format_result
+from kili.mutations.project.queries import GQL_DELETE_PROJECT
 
 from ..mutations.organization import MutationsOrganization
 from ..mutations.user.queries import GQL_RESET_PASSWORD
@@ -36,4 +37,20 @@ class KiliInternal(MutationsOrganization):
         """
         variables = {"where": {"email": email}}
         result = self.auth.client.execute(GQL_RESET_PASSWORD, variables)
+        return format_result("data", result)
+
+    @typechecked
+    def delete_project(self, project_id: str):
+        """Delete project permanently.
+        WARNING: This resolver is for internal use by Kili Technology only.
+
+        Args:
+            project_id: Identifier of the project
+
+        Returns:
+            A result object which indicates if the mutation was successful,
+                or an error message.
+        """
+        variables = {"projectID": project_id}
+        result = self.auth.client.execute(GQL_DELETE_PROJECT, variables)
         return format_result("data", result)
