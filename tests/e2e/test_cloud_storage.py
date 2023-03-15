1d199d5306b57d644fb48d875c3ae8d4be1c5fd9
Jonas Maison <Jonas1312@users.noreply.github.com>
2023-03-15 14:31:48 +0100

    chore: add tests from master

---
+++ tests/e2e/test_cloud_storage.py
@@ -0,0 +1,158 @@
+"""
+Test cloud storage methods
+"""
+
+import json
+import os
+from typing import Dict, List, Tuple
+
+import pytest
+
+from kili.client import Kili
+
+
+@pytest.fixture
+def kili() -> Kili:
+    return Kili()
+
+
+@pytest.fixture()
+def src_project(kili: Kili):
+    interface = {
+        "jobs": {
+            "JOB_0": {
+                "content": {
+                    "categories": {
+                        "OBJECT_A": {"children": [], "name": "Object A"},
+                        "OBJECT_B": {"children": [], "name": "Object B"},
+                    },
+                    "input": "radio",
+                },
+                "instruction": "Categories",
+                "isChild": False,
+                "mlTask": "CLASSIFICATION",
+                "models": {},
+                "isVisible": True,
+                "required": 1,
+            }
+        }
+    }
+
+    project = kili.create_project(
+        input_type="IMAGE",
+        json_interface=interface,
+        title="test_e2e_synchronize_cloud_storage_connection",
+        description="test_e2e_synchronize_cloud_storage_connection",
+    )
+
+    yield project
+
+    kili.delete_project(project["id"])
+
+
+def get_test_cases() -> List[Tuple[str]]:
+    """
+    KILI_TEST_DATA_INTEGRATION_ID is a json string of the form:
+    {
+        "LTS": {
+            "AWS": ["data_integration_id_1", "data_integration_id_2"],
+            "GCP": ["data_integration_id_1", "data_integration_id_2"],
+            "Azure": ["data_integration_id_1", "data_integration_id_2"],
+        },
+        "STAGING": {
+            "AWS": ["data_integration_id_1", "data_integration_id_2"],
+            ...
+        },
+        ...
+    }
+    """
+    test_cases = []
+
+    integrations_ids = os.environ.get("KILI_TEST_DATA_INTEGRATION_ID")
+    if integrations_ids is None:
+        return test_cases
+
+    integrations_ids = json.loads(integrations_ids)
+
+    for endpoint_short_name in integrations_ids:
+        for platform_name in integrations_ids[endpoint_short_name]:
+            for data_integration_id in integrations_ids[endpoint_short_name][platform_name]:
+                test_cases.append((endpoint_short_name, platform_name, data_integration_id))
+
+    return test_cases
+
+
+def is_same_endpoint(endpoint_short_name: str, endpoint_url: str) -> bool:
+    if endpoint_short_name == "LTS":
+        return "lts" in endpoint_url
+    elif endpoint_short_name == "STAGING":
+        return "staging" in endpoint_url
+    elif endpoint_short_name == "PREPROD":
+        return "preprod" in endpoint_url
+    elif endpoint_short_name == "PROD":
+        return "https://cloud" in endpoint_url
+    else:
+        raise ValueError(f"Unknown endpoint short name: {endpoint_short_name}")
+
+
+@pytest.mark.parametrize("endpoint_short_name,platform_name,data_integration_id", get_test_cases())
+def test_e2e_synchronize_cloud_storage_connection(
+    kili: Kili,
+    src_project: Dict,
+    endpoint_short_name: str,
+    platform_name: str,
+    data_integration_id: str,
+):
+    """
+    e2e test for cloud storage methods
+    """
+    if not is_same_endpoint(endpoint_short_name, kili.auth.api_endpoint):
+        pytest.skip("Skipping test because it is not the right endpoint.")
+
+    project_id = src_project["id"]
+
+    # Check that the data integration exists
+    print("Data integration used:", data_integration_id)
+    data_integrations = kili.cloud_storage_integrations(
+        status="CONNECTED", cloud_storage_integration_id=data_integration_id
+    )
+    if len(data_integrations) != 1:
+        raise ValueError("No data integration found. Cannot run test.", data_integrations)
+
+    # Create a data connection
+    data_connection_id = kili.add_cloud_storage_connection(
+        project_id=project_id, cloud_storage_integration_id=data_integration_id
+    )["id"]
+
+    # Check that the data connection has been created
+    data_connections = kili.cloud_storage_connections(
+        project_id=project_id,
+        fields=[
+            "id",
+            "dataDifferencesSummary.added",
+            "dataDifferencesSummary.removed",
+            "dataDifferencesSummary.total",
+            "isChecking",
+            "isApplyingDataDifferences",
+            "lastChecked",
+            "numberOfAssets",
+            "selectedFolders",
+            "dataIntegrationId",
+        ],
+    )
+    assert len(data_connections) == 1, data_connections
+    data_connection = data_connections[0]
+    assert data_connection["id"] == data_connection_id, data_connection
+    assert data_connection["dataIntegrationId"] == data_integration_id, data_connection
+    print("Data connection:", data_connection)
+
+    nb_assets = kili.count_assets(project_id=project_id)
+    assert nb_assets == 0, f"Expected no asset before sync. Got {nb_assets}"
+
+    kili.synchronize_cloud_storage_connection(
+        cloud_storage_connection_id=data_connection_id,
+        delete_extraneous_files=True,
+    )
+
+    nb_assets = kili.count_assets(project_id=project_id)
+    assert nb_assets > 0, "Expected at least one asset after sync."
---
+++ tests/graphql/test_graphql_client.py
@@ -0,0 +1,90 @@
+import os
+from pathlib import Path
+from unittest import mock
+
+import graphql
+import pytest
+
+from kili.exceptions import GraphQLError
+from kili.graphql.graphql_client import GraphQLClient, GraphQLClientName
+
+
+def test_graphql_client_cache_cant_get_kili_version(mocker):
+    """test when we can't get the kili version from the backend"""
+
+    mocker.patch("kili.graphql.graphql_client.Client", return_value=None)
+    mocker.patch.object(GraphQLClient, "_get_kili_app_version", return_value=None)
+
+    _ = GraphQLClient(
+        endpoint="https://",
+        api_key="nokey",
+        client_name=GraphQLClientName.SDK,
+        verify=True,
+    )
+
+
+@pytest.mark.parametrize(
+    "query",
+    [
+        "query MyQuery { my_query_is_clearly_not_valid_according_to_the_kili_schema { id } }",
+        "query { projects { this_field_does_not_exist } }",
+    ],
+)
+def test_gql_bad_query_local_validation(query, mocker):
+    """test gql validation against local schema"""
+    api_endpoint = os.getenv(
+        "KILI_API_ENDPOINT", "https://cloud.kili-technology.com/api/label/v2/graphql"
+    )
+
+    # we need to remove "Authorization" api key from the header
+    # if not, the backend will refuse the introspection query
+    mocker.patch.object(GraphQLClient, "_get_headers", return_value={})
+
+    client = GraphQLClient(
+        endpoint=api_endpoint,
+        api_key="",
+        client_name=GraphQLClientName.SDK,
+        verify=True,
+    )
+
+    with pytest.raises(GraphQLError) as exc_info:
+        client.execute(query)
+
+    assert isinstance(exc_info.value.__cause__, graphql.GraphQLError)
+
+
+def test_graphql_client_cache(mocker):
+    SCHEMA_PATH = Path.home() / ".cache" / "kili" / "graphql" / "schema.graphql"
+    mocker.patch.object(GraphQLClient, "_get_graphql_schema_path", return_value=SCHEMA_PATH)
+
+    api_endpoint = os.getenv(
+        "KILI_API_ENDPOINT", "https://cloud.kili-technology.com/api/label/v2/graphql"
+    )
+
+    # we need to remove "Authorization" api key from the header
+    # if not, the backend will refuse the introspection query
+    mocker.patch.object(GraphQLClient, "_get_headers", return_value={})
+
+    if SCHEMA_PATH.is_file():
+        SCHEMA_PATH.unlink()
+
+    _ = GraphQLClient(
+        endpoint=api_endpoint,
+        api_key="",
+        client_name=GraphQLClientName.SDK,
+        verify=True,
+    )
+    assert SCHEMA_PATH.is_file()  # schema cached
+    assert SCHEMA_PATH.stat().st_size > 0  # schema not empty
+
+    with mock.patch("kili.graphql.graphql_client.print_schema") as mocked_print_schema:
+        _ = GraphQLClient(
+            endpoint=api_endpoint,
+            api_key="",
+            client_name=GraphQLClientName.SDK,
+            verify=True,
+        )
+        mocked_print_schema.assert_not_called()
+
+    if SCHEMA_PATH.is_file():
+        SCHEMA_PATH.unlink()
