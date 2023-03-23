b9d7399c8dbf087f0973e394c37561d8384ce9cb
Jonas Maison <Jonas1312@users.noreply.github.com>
2023-03-23 09:17:15 +0100

    feat: add label parsing module

---
+++ src/kili/services/json_response/category.py
@@ -0,0 +1,92 @@
+# Feature is still under development and is not yet suitable for use by general users.
+"""Module for the "categories" key parsing of a job response."""
+from typing import Dict, List
+
+from typeguard import typechecked
+
+from .exceptions import InvalidMutationError
+
+
+class Category(Dict):
+    """Class for Category parsing."""
+
+    def __init__(self, job_interface: Dict, name: str, confidence: int = 100) -> None:
+        """Class for Category parsing.
+
+        Args:
+            name: Name of the category label.
+            confidence: Confidence of the category label.
+            job_interface: Job interface of the job.
+        """
+        super().__init__()
+        self.job_interface = job_interface
+
+        self.name = name
+        self.confidence = confidence
+
+    @property
+    def name(self) -> str:
+        """Returns the name of the category label."""
+        return self["name"]
+
+    @property
+    def confidence(self) -> int:
+        """Returns the confidence of the category label."""
+        return self["confidence"]
+
+
+class CategoryList(List):
+    """Class for the categories list parsing."""
+
+    def __init__(self, job_interface: Dict, categories_list: List[Dict]) -> None:
+        """Class for the categories list parsing.
+
+        Args:
+            categories_list: List of dicts representing categories.
+            job_interface: Job interface of the job.
+        """
+        super().__init__()
+        self.job_interface = job_interface
+
+        for category_dict in categories_list:
+            self.append(Category(**category_dict, job_interface=job_interface))
+
+    def _check_can_append_category(self) -> None:
+        input_type = self.job_interface["content"]["input"]
+        nb_classes = len(self.job_interface["content"]["categories"])
+        len_categories = len(self)
+
+        if input_type == "radio":
+            if len_categories >= 1:
+                raise InvalidMutationError(
+                    "Cannot add more than one category to a radio button classification job."
+                )
+
+        elif input_type == "checkbox":
+            if len_categories >= nb_classes:
+                raise InvalidMutationError(
+                    "Cannot add more categories than the number of classes to a checkbox"
+                    " classification job."
+                )
+
+        elif input_type == "singleDropdown":
+            if len_categories >= 1:
+                raise InvalidMutationError(
+                    "Cannot add more than one category to a single dropdown classification job."
+                )
+
+        elif input_type == "multipleDropdown":
+            if len_categories >= nb_classes:
+                raise InvalidMutationError(
+                    "Cannot add more categories than the number of classes to a multiple dropdown"
+                    " classification job."
+                )
+
+        else:
+            raise ValueError(f"Invalid input type: {input_type}")
+
+    @typechecked
+    def append(self, category: Category) -> None:
+        """Appends a category object to the CategoryList object."""
+        self._check_can_append_category()
+        return super().append(category)
