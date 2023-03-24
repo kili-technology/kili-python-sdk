a381f9a9a59f357615623048979a306d6a61a5fd
Jonas Maison <Jonas1312@users.noreply.github.com>
2023-03-24 18:20:55 +0100

    feat: add helper to create segmentation mask labels

--- src/kili/utils/labels/__init__.py
+++ src/kili/utils/labels/__init__.py
@@ -1,7 +1,13 @@
-"""Kili label creation helpers module."""
+"""Set of utils to work with labels."""
 
 from .bbox import bbox_points_to_normalized_vertices
 from .image import mask_to_vertices
+from .parse_labels import parse_labels
 from .point import point_to_normalized_point
 
-__all__ = ["bbox_points_to_normalized_vertices", "mask_to_vertices", "point_to_normalized_point"]
+__all__ = [
+    "bbox_points_to_normalized_vertices",
+    "mask_to_vertices",
+    "point_to_normalized_point",
+    "parse_labels",
+]
