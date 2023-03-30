ead4be82f3d950e20bae3b83a2eb75944c0d97b5
Jonas Maison <Jonas1312@users.noreply.github.com>
2023-03-30 11:30:13 +0200

    feat: up

--- src/kili/utils/labels/__init__.py
+++ src/kili/utils/labels/__init__.py
@@ -1,41 +1,7 @@
 """Set of utils to work with labels."""
 
-from typing import Dict, Generator, Iterable, List, overload
+from .parse_labels import parse_labels
 
-from kili.enums import InputType
-
-from ...services.label_data_parsing.label import ParsedLabel
-
-
-@overload
-def parse_labels(
-    labels: List[Dict], json_interface: Dict, input_type: InputType
-) -> List[ParsedLabel]:
-    ...
-
-
-@overload
-def parse_labels(
-    labels: Generator[Dict, None, None], json_interface: Dict, input_type: InputType
-) -> Generator[ParsedLabel, None, None]:
-    ...
-
-
-def parse_labels(
-    labels: Iterable[Dict], json_interface: Dict, input_type: InputType
-) -> Iterable[ParsedLabel]:
-    """Parse labels returned by kili.labels().
-
-    Args:
-        labels: List or generator of labels from kili.labels().
-        json_interface: Json interface of the project.
-        input_type: Type of assets of the project.
-
-    Returns:
-        Parsed labels.
-    """
-    gen = (
-        ParsedLabel(label=label, json_interface=json_interface, input_type=input_type)
-        for label in labels
-    )
-    return gen if isinstance(labels, Generator) else list(gen)
+__all__ = [
+    "parse_labels",
+]
--- tests/services/label_data_parsing/test_label_utils.py
+++ tests/services/label_data_parsing/test_label_utils.py
@@ -3,7 +3,7 @@ import json
 import pytest
 
 from kili.services.label_data_parsing.category import Category, CategoryList
-from kili.utils.labels import parse_labels
+from kili.utils.labels.parse_labels import parse_labels
 
 
 def test_parse_labels_classification():
