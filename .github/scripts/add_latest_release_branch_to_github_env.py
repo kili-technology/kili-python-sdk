4d33f31b3d1869d9f1987c9b487f36239fc1eaa2
Jonas Maison <Jonas1312@users.noreply.github.com>
2023-04-06 13:43:56 +0200

    ci: add retries for script fetching release branches from github (#1152)

--- .github/scripts/add_latest_release_branch_to_github_env.py
+++ .github/scripts/add_latest_release_branch_to_github_env.py
@@ -1,35 +1,61 @@
 """This script adds the latest release branch to the github env file."""
 import json
 import os
+import urllib.error
 import urllib.request
+from time import sleep
 
-from packaging.version import InvalidVersion, parse
+from packaging.version import InvalidVersion, Version, parse
 
 
-def parse_version(version: str):
+def parse_version(version: str) -> Version:
     try:
         return parse(version)
     except InvalidVersion:
         return parse("0.0.0")
 
 
+github_token = os.environ["GITHUB_TOKEN"]
+headers = {
+    "Authorization": f"Bearer {github_token}",
+    "Accept": "application/vnd.github+json",
+}
+
 BRANCH_PREFIX = "release/"
 per_page = 100
 page = 1
-
+retries = 0
 branch_versions = []
 while True:
-    url = f"https://api.github.com/repos/kili-technology/kili-python-sdk/branches?page={page}&per_page={per_page}&protected=true"
-    response = urllib.request.urlopen(url).read()
-    response = json.loads(response)
+    url = (
+        "https://api.github.com/repos/kili-technology/kili-python-sdk"
+        + f"/branches?page={page}&per_page={per_page}&protected=true"
+    )
+
+    try:
+        req = urllib.request.Request(url, headers=headers)
+        with urllib.request.urlopen(req) as response:
+            data = response.read()
+    except urllib.error.HTTPError as err:
+        print(f"Error while fetching branches: {err}")
+        if err.code != 403:
+            raise err
+        sleep(2**retries)
+        retries += 1
+        continue
+
+    response = json.loads(data)
+
     batch_branch_versions = [
         branch["name"].replace(BRANCH_PREFIX, "")
         for branch in response
         if branch["name"].startswith(BRANCH_PREFIX)
     ]
     branch_versions.extend(batch_branch_versions)
+
     if len(response) < per_page:
         break
+
     page += 1
 
 release_versions = sorted(branch_versions, key=parse_version)
