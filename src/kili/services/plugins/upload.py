cddb4d050cf29cce9af7bd5c5c744a4d2372426b
florianlega <104771295+florianlega@users.noreply.github.com>
2023-05-15 14:24:31 +0200

    Update src/kili/services/plugins/upload.py
    Co-authored-by: Gheorghe Tutunaru <99664787+gheorghetutunaru@users.noreply.github.com>

--- src/kili/services/plugins/upload.py
+++ src/kili/services/plugins/upload.py
@@ -309,7 +309,7 @@ class PluginUploader:
             raise PluginCreationError(
                 "There was some error during the creation of the plugin. Please check your"
                 " plugin's code and try to overwrite the plugin with a new version of the code"
-                " (you can use kili.update_plugin() for that).Hint: You can get build errors"
+                " (you can use kili.update_plugin() for that). Hint: You can get build errors"
                 f' using: kili.get_plugin_build_errors(plugin_name="{self.plugin_name}")'
             )
 
