c5bd7576d95f21a3732d246894aff6261c99b44d
Jonas Maison <Jonas1312@users.noreply.github.com>
2023-07-12 11:11:36 +0200

    test: fix

--- tests/e2e/test_projects.py
+++ tests/e2e/test_projects.py
@@ -14,17 +14,17 @@ def kili() -> Kili:
 def projects_uuid(kili: Kili):
     projects_uuid = str(uuid.uuid4())
     proj_id_archived = kili.create_project(
-        input_type="TEXT", json_interface={}, title="test_projects.py archived" + projects_uuid
+        input_type="TEXT", json_interface={}, title="test_projects.py archived " + projects_uuid
     )["id"]
     proj_id_not_archived_1 = kili.create_project(
         input_type="TEXT",
         json_interface={},
-        title="test_projects.py not archived 1" + projects_uuid,
+        title="test_projects.py not archived 1 " + projects_uuid,
     )["id"]
     proj_id_not_archived_2 = kili.create_project(
         input_type="TEXT",
         json_interface={},
-        title="test_projects.py not archived 2" + projects_uuid,
+        title="test_projects.py not archived 2 " + projects_uuid,
     )["id"]
 
     kili.archive_project(proj_id_archived)
@@ -37,18 +37,19 @@ def projects_uuid(kili: Kili):
 
 
 def test_projects_query_archived_project(kili: Kili, projects_uuid: str):
-    assert (
-        len(kili.projects(fields=["id"], search_query=projects_uuid))
-        == kili.count_projects(search_query=projects_uuid)
-        == 3
-    )
-    assert (
-        len(kili.projects(fields=["id"], search_query=projects_uuid, archived=False))
-        == kili.count_projects(search_query=projects_uuid, archived=False)
-        == 2
-    )
-    assert (
-        len(kili.projects(fields=["id"], search_query=projects_uuid, archived=True))
-        == kili.count_projects(search_query=projects_uuid, archived=True)
-        == 1
-    )
+    search_query = f"%{projects_uuid}%"
+
+    # should have 2 projects not archived
+    assert kili.count_projects(search_query=search_query, archived=False) == 2
+    projects = kili.projects(fields=["id", "title"], search_query=search_query, archived=False)
+    assert len(projects) == 2
+
+    # should have 1 project archived
+    assert kili.count_projects(search_query=search_query, archived=True) == 1
+    projects = kili.projects(fields=["id", "title"], search_query=search_query, archived=True)
+    assert len(projects) == 1
+
+    # should have 3 projects in total
+    assert kili.count_projects(search_query=search_query) == 3
+    projects = kili.projects(fields=["id", "title"], search_query=search_query)
+    assert len(projects) == 3
