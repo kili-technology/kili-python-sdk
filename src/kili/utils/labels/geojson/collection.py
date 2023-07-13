782d3435456406896343b6a474def17748ed864a
Jonas Maison <Jonas1312@users.noreply.github.com>
2023-07-13 16:28:11 +0200

    docs:

--- src/kili/utils/labels/geojson/collection.py
+++ src/kili/utils/labels/geojson/collection.py
@@ -1,3 +1,4 @@
+import warnings
 from typing import Any, Dict, List, Literal, Sequence, Union
 
 from .bbox import kili_bbox_annotation_to_geojson_polygon_feature
@@ -68,6 +69,44 @@ def features_to_feature_collection(
 def kili_label_to_feature_collection(
     json_response: Dict[str, Any]
 ) -> Dict[Literal["type", "features"], Union[str, List[Dict]]]:
+    """Convert a Kili label to a Geojson feature collection.
+
+    Args:
+        json_response: a Kili label.
+
+    Returns:
+        A Geojson feature collection.
+
+    !!! Example
+        ```python
+        >>> json_response = {
+            'job_1': {
+                'annotations': [...]
+            },
+            'job_2': {
+                'annotations': [...]
+            }
+        }
+        >>> kili_label_to_feature_collection(json_response)
+        {
+            'type': 'FeatureCollection',
+            'features': [
+                {
+                    'type': 'Feature',
+                    'geometry': {
+                        ...
+                    }
+                },
+                {
+                    'type': 'Feature',
+                    'geometry': {
+                        ...
+                    }
+                }
+            ]
+        }
+        ```
+    """
     features = []
 
     annotation_tool_to_converter = {
@@ -78,15 +117,30 @@ def kili_label_to_feature_collection(
         "semantic": kili_segmentation_annotation_to_geojson_polygon_feature,
     }
 
+    jobs_skipped = []
+    ann_tools_not_supported = set()
     for job_name, job_response in json_response.items():
-        if "annotations" in job_response:
-            for ann in job_response["annotations"]:
-                annotation_tool = ann.get("type")
-                if annotation_tool not in annotation_tool_to_converter:
-                    continue
+        if "annotations" not in job_response:
+            jobs_skipped.append(job_name)
+            continue
 
-                converter = annotation_tool_to_converter[annotation_tool]
-                feature = converter(ann, job_name=job_name)
-                features.append(feature)
+        for ann in job_response["annotations"]:
+            annotation_tool = ann.get("type")
+            if annotation_tool not in annotation_tool_to_converter:
+                ann_tools_not_supported.add(annotation_tool)
+                continue
 
+            converter = annotation_tool_to_converter[annotation_tool]
+            feature = converter(ann, job_name=job_name)
+            features.append(feature)
+
+    if jobs_skipped:
+        warnings.warn(
+            f"Jobs {jobs_skipped} do not have annotations and will be skipped.", stacklevel=2
+        )
+    if ann_tools_not_supported:
+        warnings.warn(
+            f"Annotation tools {ann_tools_not_supported} are not supported and will be skipped.",
+            stacklevel=2,
+        )
     return features_to_feature_collection(features)
