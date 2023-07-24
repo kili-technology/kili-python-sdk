e5e300f1e2b49a06544f0632bddcaef4c9a220cc
Pierre Leveau <pierre.leveau@kili-technology.com>
2023-07-24 17:22:08 +0200

    test: tentative resortion of previous test

--- tests/unit/core/test_authentication.py
+++ tests/unit/core/test_authentication.py
@@ -1,6 +1,7 @@
 import datetime
 import platform
 import warnings
+from unittest.mock import patch
 
 import pytest
 import pytest_mock
@@ -15,24 +16,21 @@ def test__check_expiry_of_key_is_close(mocker: pytest_mock.MockerFixture):
     mocker.patch.object(Kili, "_check_api_key_valid", return_value=True)
     mocker.patch.object(Kili, "get_user", return_value={"id": "id", "email": "email"})
     mocker.patch("kili.client.GraphQLClient")
-    mocker.patch.object(
+
+    with patch.object(
         APIKeyQuery, "__call__", return_value=iter([{"expiryDate": "2021-01-01T00:00:00.000Z"}])
-    )
+    ):
+        with pytest.warns(UserWarning, match="Your api key will be deprecated on"):
+            _ = Kili(api_key="", api_endpoint="", client_name=GraphQLClientName.SDK)
 
-    # with pytest.warns(UserWarning, match="Your api key will be deprecated on"):
-    #     _ = Kili(api_key="", api_endpoint="", client_name=GraphQLClientName.SDK)
-
-
-# def test__check_api_
-#     current = datetime.datetime.now()
-
-#     mocker.patch.object(
-#         APIKeyQuery,
-#         "__call__",
-#         return_value=iter(
-#             [{"expiryDate": f"{current.year+1}-{current.month}-{current.day}T09:54:19.071Z"}]
-#         ),
-#     )
-#     with warnings.catch_warnings():
-#         warnings.simplefilter("error")
-#         _ = Kili(api_key="", api_endpoint="", client_name=GraphQLClientName.SDK)
+    current = datetime.datetime.now()
+    with patch.object(
+        APIKeyQuery,
+        "__call__",
+        return_value=iter(
+            [{"expiryDate": f"{current.year+1}-{current.month}-{current.day}T09:54:19.071Z"}]
+        ),
+    ):
+        with warnings.catch_warnings():
+            warnings.simplefilter("error")
+            _ = Kili(api_key="", api_endpoint="", client_name=GraphQLClientName.SDK)
