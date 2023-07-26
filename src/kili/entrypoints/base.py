24a19208baedc2138a2e0c6c5a8cd3c1a24b2d59
Pierre Leveau <pierre.leveau@kili-technology.com>
2023-07-26 16:06:33 +0200

    fix: remove implem of format result in base mixin

--- src/kili/entrypoints/base.py
+++ src/kili/entrypoints/base.py
@@ -1,15 +1,15 @@
 """Base class for entrypoints dealing with GraphQL operations."""
+import abc
 from typing import Optional, Type, TypeVar
 
 import requests
 
 from kili.core.graphql.graphql_client import GraphQLClient
-from kili.core.helpers import format_result
 
 T = TypeVar("T")
 
 
-class BaseOperationEntrypointMixin:
+class BaseOperationEntrypointMixin(abc.ABC):
     """Base class for entrypoints dealing with GraphQL operations."""
 
     graphql_client: GraphQLClient
@@ -17,4 +17,4 @@ class BaseOperationEntrypointMixin:
 
     def format_result(self, name: str, result: dict, object_: Optional[Type[T]] = None) -> T:
         """Format the result of a GraphQL operations."""
-        return format_result(name, result, object_, self.http_client)
+        raise NotImplementedError
