c69986c7fc15f457edf076e436bd5209ad49af2e
Jonas Maison <Jonas1312@users.noreply.github.com>
2023-07-28 15:20:12 +0200

    feat: add `kili.delete_labels()`

--- tests/integration/entrypoints/mutations/test_labels.py
+++ tests/integration/entrypoints/mutations/test_labels.py
@@ -14,3 +14,13 @@ def test_delete_labels(mocker: pytest_mock.MockerFixture):
         GQL_DELETE_LABELS,
         {"ids": ["id1", "id2"]},
     )
+
+
+def test_delete_labels_big_batch(mocker: pytest_mock.MockerFixture):
+    kili = MutationsLabel()
+    kili.graphql_client = mocker.MagicMock()
+    kili.http_client = mocker.MagicMock()
+
+    kili.delete_labels(ids=["id1"] * 101)
+
+    assert kili.graphql_client.execute.call_count == 2
