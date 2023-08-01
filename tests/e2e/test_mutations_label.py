897a044c6270366a6bc19dec5bf88c20aceeb198
Jonas Maison <Jonas1312@users.noreply.github.com>
2023-08-01 09:22:58 +0200

    test: disable delete_labels e2e test

--- tests/e2e/test_mutations_label.py
+++ tests/e2e/test_mutations_label.py
@@ -52,6 +52,7 @@ def project(kili: Kili):
     kili.delete_project(project["id"])
 
 
+@pytest.mark.skip(reason="not available on staging yet")
 def test_e2e_delete_labels(kili: Kili, project: Dict):
     # Given
     labels = kili.labels(project_id=project["id"], fields=["id"])
