4f2edd8a545a7d0dccaecfe933ae9caeba802e43
Jonas Maison <Jonas1312@users.noreply.github.com>
2023-08-09 10:49:58 +0200

    feat: ask for api key if not provided when init Kili client

--- tests/test_client.py
+++ tests/test_client.py
@@ -11,6 +11,15 @@ from kili.client import Kili
 from kili.exceptions import AuthenticationFailed
 
 
+def test_no_api_key(mocker, monkeypatch):
+    """Test fail because no api key is found."""
+    mocker.patch("kili.client.requests")
+    mocker.patch("kili.client.getpass.getpass", return_value="")
+    monkeypatch.delenv("KILI_API_KEY", raising=False)
+    with pytest.raises(AuthenticationFailed):
+        _ = Kili()
+
+
 def test_wrong_api_key(mocker, monkeypatch):
     """Test obfuscation of api key."""
     mocker.patch("kili.client.requests")
