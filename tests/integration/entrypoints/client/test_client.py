0892f0757b71b5fb597d4e9d3dcc38fedeb1ee6c
Jonas Maison <Jonas1312@users.noreply.github.com>
2023-08-21 13:24:27 +0200

    test: re-organize test unit integration e2e

--- tests/integration/entrypoints/client/test_client.py
+++ tests/integration/entrypoints/client/test_client.py
@@ -50,14 +50,16 @@ def prepare_cache_dir():
         shutil.rmtree(kili_cache_dir)
 
 
-def test_write_to_disk_without_permissions_not_crash(prepare_cache_dir):
+@patch.dict(os.environ, {"KILI_API_KEY": "fake_key"})
+def test_write_to_disk_without_permissions_not_crash(
+    prepare_cache_dir, mocker: pytest_mock.MockerFixture
+):
     """Test that we can still use kili even if we don't have write permissions."""
-    # mocker.patch("io.open", side_effect=PermissionError("No write permissions"))
-    # mocker.patch("os.mkdir", side_effect=PermissionError("No write permissions"))
-    # mocker.patch.object(Path, "mkdir", side_effect=PermissionError("No write permissions"))
-    # mocker.patch.object(Kili, "_check_api_key_valid")
-    # mocker.patch.object(Kili, "_check_expiry_of_key_is_close")
-    # monkeypatch.setenv("KILI_API_KEY", "fake_key")
+    mocker.patch("io.open", side_effect=PermissionError("No write permissions"))
+    mocker.patch("os.mkdir", side_effect=PermissionError("No write permissions"))
+    mocker.patch.object(Path, "mkdir", side_effect=PermissionError("No write permissions"))
+    mocker.patch.object(Kili, "_check_api_key_valid")
+    mocker.patch.object(Kili, "_check_expiry_of_key_is_close")
     _ = prepare_cache_dir
 
     _ = Kili(graphql_client_params={"enable_schema_caching": False})
