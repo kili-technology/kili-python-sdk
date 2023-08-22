afdca36dc4861ddf48cddfc97cc74e3c1b39390d
Jonas Maison <Jonas1312@users.noreply.github.com>
2023-08-22 15:04:28 +0200

    fix: add status filter to projectUsers query

--- tests/e2e/test_query_project_users.py
+++ tests/e2e/test_query_project_users.py
@@ -19,21 +19,25 @@ def project_id_suspended_user_email(kili: Kili):
     )
 
     # We add some users to both the orga and the project
-    for role in ("LABELER", "TEAM_MANAGER", "REVIEWER", "ADMIN"):
-        kili.append_to_roles(
-            project_id=project["id"],
-            user_email=f"john.doe+{role}@kili-technology.com",
-            role=role,
-        )
+    # for role in ("LABELER", "TEAM_MANAGER", "REVIEWER", "ADMIN"):
+    #     kili.append_to_roles(
+    #         project_id=project["id"],
+    #         user_email=f"john.doe+{role}@kili-technology.com",
+    #         role=role,
+    #     )
 
-    # add a user that he will desactivate
+    # add a user that we will desactivate
     suspended_user_email = f"john.doe{uuid.uuid4()}+desactivated@kili-technology.com"
     kili.append_to_roles(
         project_id=project["id"],
         user_email=suspended_user_email,
         role="LABELER",
     )
+    # project_users = kili.project_users(project_id=project["id"], fields=["id", "user.email"])
+    # user = [user for user in project_users if user["user"]["email"] == suspended_user_email][0]
+
     kili.update_properties_in_user(email=suspended_user_email, activated=False)
+    # kili.delete_from_roles(user["id"])
     # query = """
     #     mutation updatePropertiesInProjectUser($data: ProjectUserData!, $where: ProjectUserWhere!) {
     #         data: updatePropertiesInProjectUser(data: $data, where: $where) {
@@ -60,15 +64,33 @@ def test_given_project_when_querying_project_users_it_works(
     # Given
     project_id, suspended_user_email = project_id_suspended_user_email
     api_user = kili.get_user()
+    fields = ["activated", "deletedAt", "id", "role", "user.email", "user.id", "status"]
 
     # When
-    fields = ["activated", "deletedAt", "id", "role", "user.email", "user.id", "status"]
-    # users = kili.project_users(project_id=project_id, activated=True)
-    # users = kili.project_users(project_id=project_id, activated=None)
-    # users = kili.project_users(project_id=project_id, activated=False)
-    users = kili.project_users(project_id=project_id, fields=fields, status="ACTIVATED")
-    users = kili.project_users(project_id=project_id, fields=fields, status="ORG_ADMIN")
-    users = kili.project_users(project_id=project_id, fields=fields, status="ORG_SUSPENDED")
+    all_users = kili.project_users(project_id=project_id, fields=fields, status=None)
 
     # Then
-    pass
+    assert len(all_users) > 0
+
+    # When
+    activated_users = kili.project_users(project_id=project_id, fields=fields, status="ACTIVATED")
+
+    # Then, only one activated user: the api user
+    for proj_user in activated_users:
+        assert proj_user["user"]["email"] in {api_user["email"]}
+
+    # When
+    admin_users = kili.project_users(project_id=project_id, fields=fields, status="ORG_ADMIN")
+
+    # Then, admin users are not api user or disabled user
+    for proj_user in admin_users:
+        assert proj_user["user"]["email"] not in {api_user["email"], suspended_user_email}
+
+    # When
+    disabled_users = kili.project_users(
+        project_id=project_id, fields=fields, status="ORG_SUSPENDED"
+    )
+
+    # Then, only one disabled user
+    for proj_user in disabled_users:
+        assert proj_user["user"]["email"] in {suspended_user_email}
