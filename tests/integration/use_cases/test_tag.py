3ad83e32d8b01d814f578af48178d37b0f0a22b5
Jonas Maison <Jonas1312@users.noreply.github.com>
2023-08-31 08:49:26 +0200

    fix:

--- tests/integration/use_cases/test_tag.py
+++ tests/integration/use_cases/test_tag.py
@@ -34,3 +34,25 @@ def test_get_tags_by_project(mocker: pytest_mock.MockerFixture):
         " }\n        }\n        ",
         {"projectId": "fake_proj_id"},
     )
+
+
+def test_tag_project(mocker: pytest_mock.MockerFixture):
+    kili = TagClientMethods()
+    kili.kili_api_gateway = KiliAPIGateway(
+        graphql_client=mocker.MagicMock(), http_client=mocker.MagicMock()
+    )
+    kili.kili_api_gateway.list_tags_by_org = mocker.MagicMock(
+        return_value=[
+            {"id": "tag1_id", "label": "tag1"},
+            {"id": "tag2_id", "label": "tag2"},
+        ]
+    )
+
+    kili.tag_project(project_id="fake_proj_id", tags=["tag1", "tag2_id"])
+
+    assert kili.kili_api_gateway.graphql_client.execute.call_count == 2
+    kili.kili_api_gateway.graphql_client.execute.assert_called_with(
+        "\nmutation checkTag($data: CheckedTagData!) {\n    data: checkTag(data: $data) {\n       "
+        " id\n    }\n}\n",
+        {"data": {"tagId": "tag2_id", "projectId": "fake_proj_id"}},
+    )
