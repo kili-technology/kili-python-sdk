cc5924870beef4620d04cbbccc65649e0eef0baa
Gheorghe Tutunaru <gheorghe.tutunaru@kili-technology.com>
2023-09-04 10:30:11 +0200

    docs: ✏️ update docstring of on_project_updated

--- src/kili/services/plugins/model.py
+++ src/kili/services/plugins/model.py
@@ -148,7 +148,7 @@ class PluginCore:
         Args:
             settings_updated: Settings updated on the project a list of
                 dictionary containing the following fields:
-                `key`, `newValue`, `oldValue`few.
+                `key`, `newValue`, `oldValue`.
                 !!! note
                     key is one of the following: 'canNavigateBetweenAssets',
                     'canSkipAsset', 'consensusTotCoverage', 'description',
@@ -159,7 +159,7 @@ class PluginCore:
         !!! example
             ```python
             def on_project_updated(self, settings_updated: List[Dict]):
-                for setting in settings_updated
+                for setting in settings_updated:
                     self.logger.info(setting)
                     # this will print:
                     # {'key': 'description', 'newValue': 'new desc', 'oldValue': 'old desc'}
