89d8c13bb29c5f7a2695f2eff5cd6f5546886474
Jonas Maison <Jonas1312@users.noreply.github.com>
2023-09-08 11:06:07 +0200

    feat:

--- src/kili/adapters/kili_api_gateway/project/formatters.py
+++ src/kili/adapters/kili_api_gateway/project/formatters.py
@@ -9,7 +9,7 @@ PROJECT_JSON_FIELDS = ("jsonInterface",)
 
 
 def load_project_json_fields(project: Dict, fields: ListOrTuple[str]) -> Dict:
-    """Load json fields of an project."""
+    """Load json fields of a project."""
     if "jsonInterface" in fields and isinstance(project.get("jsonInterface"), str):
         project["jsonInterface"] = json.loads(project["jsonInterface"])
     return project
