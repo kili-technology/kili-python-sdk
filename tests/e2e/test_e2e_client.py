240b14fbe59aaade5cb7821f88be5159d8e4a6cd
Jonas Maison <Jonas1312@users.noreply.github.com>
2023-09-12 13:19:15 +0200

    test: increase Kili client init timeout for windows

--- tests/e2e/test_e2e_client.py
+++ tests/e2e/test_e2e_client.py
@@ -1,3 +1,5 @@
+import os
+
 import pytest_mock
 from pyinstrument.profiler import Profiler
 
@@ -21,4 +23,5 @@ def test_client_init_not_too_long_with_checks_disabled(mocker: pytest_mock.Mocke
         _ = Kili()
 
     time_spent = profiler.last_session.duration  # type: ignore
-    assert time_spent < 1, profiler.output_text(unicode=False)
+    limit = 2 if os.name == "nt" else 1  # Windows is slower
+    assert time_spent < limit, profiler.output_text(unicode=False)
