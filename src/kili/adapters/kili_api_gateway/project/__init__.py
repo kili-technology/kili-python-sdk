2b6d50a2dd6c6ab9c5a8980f5f47a2b7464e5fff
Jonas Maison <Jonas1312@users.noreply.github.com>
2023-09-14 10:21:39 +0200

    fix: Update src/kili/adapters/kili_api_gateway/project/__init__.py
    Co-authored-by: theodu <82399630+theodu@users.noreply.github.com>

--- src/kili/adapters/kili_api_gateway/project/__init__.py
+++ src/kili/adapters/kili_api_gateway/project/__init__.py
@@ -93,7 +93,7 @@ class ProjectOperationMixin(BaseOperationMixin):
     ) -> Dict:
         """Update properties in a project."""
         fragment = fragment_builder(fields)
-        query = get_update_properties_in_project_mutation(fragment)
+        mutation = get_update_properties_in_project_mutation(fragment)
         data = project_data_mapper(data=project_data)
         variables = {"data": data, "where": {"id": project_id}}
         result = self.graphql_client.execute(query, variables)
