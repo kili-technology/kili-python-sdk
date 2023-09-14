a85af2361fe139234471a78e5813515d5203893f
Pierre Leveau <pierre.leveau@kili-technology.com>
2023-09-14 20:09:15 +0200

    style: pylint fixes

--- src/kili/use_cases/utils/__init__.py
+++ src/kili/use_cases/utils/__init__.py
@@ -1,3 +1,4 @@
+"""Use cases utilities."""
 from .use_cases_utils import UseCasesUtils
 
 __all__ = ["UseCasesUtils"]
--- src/kili/use_cases/utils/use_cases_utils.py
+++ src/kili/use_cases/utils/use_cases_utils.py
@@ -1,5 +1,6 @@
+"""Utils for use cases."""
 from itertools import chain
-from typing import Dict, Generator, List, Optional
+from typing import Dict, List, Optional
 
 from kili.adapters.kili_api_gateway import KiliAPIGateway
 from kili.adapters.kili_api_gateway.helpers.queries import QueryOptions
@@ -10,7 +11,10 @@ from kili.exceptions import NotFound
 
 
 class UseCasesUtils:
+    """Utils for use cases."""
+
     def __init__(self, kili_api_gateway: KiliAPIGateway) -> None:
+        """Init."""
         self.kili_api_gateway = kili_api_gateway
 
     def get_asset_ids_or_throw_error(
@@ -22,13 +26,11 @@ class UseCasesUtils:
         """Check if external id to internal id conversion is valid and needed."""
         check_asset_identifier_arguments(project_id, asset_ids, external_ids)
 
-        if asset_ids is None:
-            id_map = self.infer_ids_from_external_ids(
-                external_ids, project_id  # type: ignore
-            )  # type: ignore
-            asset_ids = [id_map[id] for id in external_ids]  # type: ignore
+        if asset_ids is None and external_ids is not None and project_id is not None:
+            id_map = self.infer_ids_from_external_ids(external_ids, project_id)
+            asset_ids = [id_map[ext_id] for ext_id in external_ids]
 
-        return asset_ids
+        return asset_ids  # type: ignore
 
     def infer_ids_from_external_ids(
         self, asset_external_ids: List[str], project_id: str
@@ -36,7 +38,6 @@ class UseCasesUtils:
         """Infer asset ids from their external ids and project Id.
 
         Args:
-            kili_api_gateway: Kili API gateway
             asset_external_ids: asset external ids
             project_id: project id
 
@@ -64,17 +65,15 @@ class UseCasesUtils:
             )
         return id_map
 
-    def _build_id_map(self, asset_external_ids, project_id):
-        assets_generators: List[Generator[Dict, None, None]] = []
-        # query all assets by external ids batches when there are too many
-        for external_ids_batch in pagination.BatchIteratorBuilder(asset_external_ids, 1000):
-            assets_generators.append(
-                self.kili_api_gateway.list_assets(
-                    AssetFilters(project_id, external_id_strictly_in=external_ids_batch),
-                    ["id", "externalId"],
-                    QueryOptions(disable_tqdm=True),
-                )
+    def _build_id_map(self, asset_external_ids: List[str], project_id: str) -> Dict[str, str]:
+        assets_generators = (
+            self.kili_api_gateway.list_assets(
+                AssetFilters(project_id, external_id_strictly_in=external_ids_batch),
+                ["id", "externalId"],
+                QueryOptions(disable_tqdm=True),
             )
+            for external_ids_batch in pagination.BatchIteratorBuilder(asset_external_ids, 1000)
+        )
         assets = chain(*assets_generators)
         id_map: Dict[str, str] = {}
         asset_external_ids_set = set(asset_external_ids)
