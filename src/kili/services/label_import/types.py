e3274449819fa6172cb1d90fce328a220bddcdfd
Jonas Maison <Jonas1312@users.noreply.github.com>
2023-09-15 12:16:47 +0200

    chore: remove pydantic warnings

--- src/kili/services/label_import/types.py
+++ src/kili/services/label_import/types.py
@@ -18,24 +18,19 @@ class ClientInputLabelData(BaseModel, extra="forbid"):
 
 
 class ClientInputLabelsValidator(BaseModel, extra="forbid"):
-    """Validates the data about a label to append."""
+    """Validates the data about labels to append."""
 
     labels: List[Dict]
 
     @field_validator("labels")
     @classmethod
     def label_validator(cls, labels: List[Dict]) -> List[Dict]:
-        """Validate the data of one label."""
+        """Validate the labels."""
         for i, _ in enumerate(labels):
             if labels[i].get("asset_external_id") is None and labels[i].get("asset_id") is None:
                 raise ValueError("You must either provide the `asset_id` or `external_id`.")
             labels[i] = ClientInputLabelData(**labels[i]).model_dump()
-        return labels
 
-    @field_validator("labels")
-    @classmethod
-    def all_labels_use_the_same_asset_identifier(cls, labels: List[Dict]) -> List[Dict]:
-        """Validate the data of all labels."""
         if not all(label.get("asset_id") for label in labels) and not all(
             label.get("asset_external_id") for label in labels
         ):
