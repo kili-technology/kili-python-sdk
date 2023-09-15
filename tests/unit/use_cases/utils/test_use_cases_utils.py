6a7c23fb29b30b24b401dd492bec9c9678074c6a
Pierre Leveau <pierre.leveau@kili-technology.com>
2023-09-15 16:17:20 +0200

    test: better fixtures

--- tests/unit/use_cases/utils/test_use_cases_utils.py
+++ tests/unit/use_cases/utils/test_use_cases_utils.py
@@ -1,4 +1,5 @@
 import pytest
+import pytest_mock
 
 from kili.adapters.kili_api_gateway import KiliAPIGateway
 from kili.domain.asset import AssetExternalId
@@ -9,23 +10,26 @@ from tests.fakes.fake_kili import mocked_AssetQuery, mocked_AssetQuery_count
 
 @pytest.mark.parametrize(
     ("name", "asset_count"),
-    (
+    [
         ("Given 150 external ids, the id <-> external_id mapping is correct", 150),
         ("Given 2400 external ids, the id <-> external_id mapping is correct", 2400),
-    ),
+    ],
 )
 def test__build_id_map_when_project_has_less_than_2000_assets(
-    mocker, name, asset_count, graphql_client, http_client
+    mocker: pytest_mock.MockerFixture, name: str, asset_count: int, kili_api_gateway: KiliAPIGateway
 ):
     _ = name
-    kili_api_gateway = mocker.MagicMock(spec=KiliAPIGateway)
-    kili_api_gateway.graphql_client = graphql_client
-    kili_api_gateway.http_client = http_client
+    # Given
     kili_api_gateway.list_assets.side_effect = mocked_AssetQuery
     kili_api_gateway.count_assets = mocker.MagicMock(side_effect=mocked_AssetQuery_count)
 
     asset_external_ids = [AssetExternalId(f"ext-{i}") for i in range(asset_count)]
     project_id = ProjectId("object_detection_2500_assets")
-    id_map = UseCasesUtils(kili_api_gateway)._build_id_map(asset_external_ids, project_id)
 
+    # When
+    id_map = UseCasesUtils(kili_api_gateway)._build_id_map(  # pylint: disable=protected-access
+        asset_external_ids, project_id
+    )
+
+    # Then
     assert id_map == {f"ext-{i}": f"{i}" for i in range(asset_count)}
