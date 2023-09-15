cfe29f9ed69bad7e9c25a2ce8a7bb75b1530a7d7
Jonas Maison <Jonas1312@users.noreply.github.com>
2023-09-15 11:14:40 +0200

    chore: remove pydantic warnings

--- src/kili/services/label_import/types.py
+++ src/kili/services/label_import/types.py
@@ -1,13 +1,13 @@
 """Types specific to import."""
-from typing import Dict, List, Literal, NewType, Optional
+from typing import Any, Dict, List, Literal, NewType, Optional
 
-from pydantic import BaseModel, Extra, StrictInt, StrictStr, validator
+from pydantic import BaseModel, StrictInt, StrictStr, field_validator
 
 Classes = NewType("Classes", Dict[int, str])
 LabelFormat = Literal["yolo_v4", "yolo_v5", "yolo_v7", "kili", "raw"]
 
 
-class ClientInputLabelData(BaseModel, extra=Extra.forbid):
+class ClientInputLabelData(BaseModel, extra="forbid"):
     """Data about a label to append, given by client in client-side function."""
 
     asset_id: Optional[StrictStr] = None
@@ -17,21 +17,23 @@ class ClientInputLabelData(BaseModel, extra=Extra.forbid):
     seconds_to_label: Optional[StrictInt] = None
 
 
-class ClientInputLabelsValidator(BaseModel, extra=Extra.forbid):
+class ClientInputLabelsValidator(BaseModel, extra="forbid"):
     """Validates the data about a label to append."""
 
     labels: List[Dict]
 
-    @validator("labels", each_item=True, pre=True)
-    def label_validator(cls, label):  # pylint: disable=no-self-argument
+    @field_validator("labels")
+    @classmethod
+    def label_validator(cls, label: Dict) -> Dict[str, Any]:
         """Validate the data of one label."""
         if label.get("asset_external_id") is None and label.get("asset_id") is None:
             raise ValueError("You must either provide the asset_id or external_id")
-        return ClientInputLabelData(**label).dict()
+        return ClientInputLabelData(**label).model_dump()
 
-    @validator("labels")
-    def all_labels_use_the_same_asset_identifier(cls, labels):  # pylint: disable=no-self-argument
-        """Validate the data of one label."""
+    @field_validator("labels")
+    @classmethod
+    def all_labels_use_the_same_asset_identifier(cls, labels: List[Dict]):
+        """Validate the data of all labels."""
         if not all(label.get("asset_id") for label in labels) and not all(
             label.get("asset_external_id") for label in labels
         ):
