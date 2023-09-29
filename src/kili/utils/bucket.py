f1f25b398ef71a044aa976e7e2b6d4e9a9b29ba9
theodu <82399630+theodu@users.noreply.github.com>
2023-09-29 10:20:54 +0200

    fix: remove timeout for file upload when uploading file to buckets (#1493)
    Co-authored-by: theodu <theo.dullin@kili-technology.com>

--- src/kili/utils/bucket.py
+++ src/kili/utils/bucket.py
@@ -62,8 +62,8 @@ def upload_data_via_rest(
     url_to_use_for_upload = url_with_id.split("&id=")[0]
     if "blob.core.windows.net" in url_to_use_for_upload:
         headers["x-ms-blob-type"] = "BlockBlob"
-
-    response = http_client.put(url_to_use_for_upload, data=data, headers=headers, timeout=30)
+    # Do we not put a timeout here because it can take an arbitrary long time (ML-1395)
+    response = http_client.put(url_to_use_for_upload, data=data, headers=headers)
     response.raise_for_status()
     return url_with_id
 
