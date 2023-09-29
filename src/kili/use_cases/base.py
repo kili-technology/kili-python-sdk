5924581845e061793ed318e882c38861cea72f88
theodu <theo.dullin@kili-technology.com>
2023-09-29 11:53:16 +0200

    feat: make BaseUseCases an abstract class

--- src/kili/use_cases/base.py
+++ src/kili/use_cases/base.py
@@ -1,8 +1,10 @@
 """Module with base class for use cases."""
+from abc import ABC
+
 from kili.adapters.kili_api_gateway import KiliAPIGateway
 
 
-class BaseUseCases:
+class BaseUseCases(ABC):
     """Base Use Cases."""
 
     def __init__(self, kili_api_gateway: KiliAPIGateway) -> None:
