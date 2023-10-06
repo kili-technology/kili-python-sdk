241be6a9c50238f929acb801a55b3f7bf92e11e0
Pierre Leveau <pierre.leveau@kili-technology.com>
2023-10-06 11:17:58 +0200

    docs: document fixtures

--- tests/e2e/conftest.py
+++ tests/e2e/conftest.py
@@ -7,9 +7,11 @@ from kili.client import Kili
 
 @pytest.fixture(scope="session")
 def kili() -> Kili:
+    """Kili client with user rights."""
     return Kili()
 
 
 @pytest.fixture(scope="session")
 def kili_admin() -> Kili:
+    """Kili client with admin rights."""
     return Kili(api_key=os.environ["KILI_API_KEY_ADMIN"])
