7a8e5a75acb02caf64d9d072d4f55436b3363706
Jonas Maison <Jonas1312@users.noreply.github.com>
2023-10-23 19:19:36 +0200

    Update test_label.py

--- tests/integration/presentation/test_label.py
+++ tests/integration/presentation/test_label.py
@@ -9,7 +9,7 @@ from kili.utils.labels.parsing import ParsedLabel
 
 def test_given_kili_client_when_fetching_labels_then_i_get_correct_type(mocker):
     """This test does not check types at runtime, but rather during pyright type checking."""
-    mocker.patch.object(Kili, "__init__")
+    mocker.patch.object(Kili, "__init__", return_value=None)
     mocker.patch.object(LabelClientMethods, "labels")
     assert_type(Kili().labels("project_id"), List[Dict])
     assert_type(Kili().labels("project_id", as_generator=True), Generator[Dict, None, None])
