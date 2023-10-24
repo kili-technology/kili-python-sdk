1bbacdbf389274eed23b0e4b1f9bde56124297b8
Jonas Maison <Jonas1312@users.noreply.github.com>
2023-10-24 09:33:58 +0200

    refactor: labels

--- tests/integration/presentation/test_label.py
+++ tests/integration/presentation/test_label.py
@@ -1,9 +1,12 @@
 from typing import Dict, Generator, List
 
+import pytest_mock
 from typing_extensions import assert_type
 
+from kili.adapters.kili_api_gateway import KiliAPIGateway
 from kili.client import Kili
 from kili.presentation.client.label import LabelClientMethods
+from kili.use_cases.label import LabelUseCases
 from kili.utils.labels.parsing import ParsedLabel
 
 
@@ -18,3 +21,35 @@ def test_given_kili_client_when_fetching_labels_then_i_get_correct_type(mocker):
         Kili().labels("project_id", output_format="parsed_label", as_generator=True),
         Generator[ParsedLabel, None, None],
     )
+
+
+def test_given_kili_client_when_fetching_prediction_labels_then_it_calls_proper_use_case(
+    mocker: pytest_mock.MockerFixture, kili_api_gateway: KiliAPIGateway
+):
+    mocked_use_cases = mocker.patch.object(LabelUseCases, "list_labels")
+
+    # Given
+    kili = LabelClientMethods()
+    kili.kili_api_gateway = kili_api_gateway
+
+    # When
+    _ = kili.predictions("fake_proj_id")
+
+    # Then
+    assert mocked_use_cases.call_count == 1
+
+
+def test_given_kili_client_when_fetching_inference_labels_then_it_calls_proper_use_case(
+    mocker: pytest_mock.MockerFixture, kili_api_gateway: KiliAPIGateway
+):
+    mocked_use_cases = mocker.patch.object(LabelUseCases, "list_labels")
+
+    # Given
+    kili = LabelClientMethods()
+    kili.kili_api_gateway = kili_api_gateway
+
+    # When
+    _ = kili.inferences("fake_proj_id")
+
+    # Then
+    assert mocked_use_cases.call_count == 1
