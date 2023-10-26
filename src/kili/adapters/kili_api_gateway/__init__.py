3e4961adf5a38055bb2813e4d03a37cdda09daa9
Jonas Maison <Jonas1312@users.noreply.github.com>
2023-10-26 11:14:47 +0200

    refactor: notification queries

--- src/kili/adapters/kili_api_gateway/__init__.py
+++ src/kili/adapters/kili_api_gateway/__init__.py
@@ -6,7 +6,7 @@ from kili.adapters.kili_api_gateway.asset import AssetOperationMixin
 from kili.adapters.kili_api_gateway.cloud_storage import CloudStorageOperationMixin
 from kili.adapters.kili_api_gateway.issue import IssueOperationMixin
 from kili.adapters.kili_api_gateway.label.operations_mixin import LabelOperationMixin
-from kili.adapters.kili_api_gateway.notification.operation_mixin import (
+from kili.adapters.kili_api_gateway.notification.operations_mixin import (
     NotificationOperationMixin,
 )
 from kili.adapters.kili_api_gateway.organization.operations_mixin import (
