a4b95f762265eae5cdc4498c02d1f544a2a64648
Jonas Maison <Jonas1312@users.noreply.github.com>
2023-10-27 14:58:42 +0200

    refactor: project queries

--- tests/unit/services/asset_import/test_import_text.py
+++ tests/unit/services/asset_import/test_import_text.py
@@ -16,9 +16,9 @@ from tests.unit.services.asset_import.mocks import (
 @patch("kili.utils.bucket.request_signed_urls", mocked_request_signed_urls)
 @patch("kili.utils.bucket.upload_data_via_rest", mocked_upload_data_via_rest)
 @patch("kili.utils.bucket.generate_unique_id", mocked_unique_id)
-# @patch.object(ProjectQuery, "__call__", side_effect=lambda *args: [{"inputType": "TEXT"}])
 class TextTestCase(ImportTestCase):
     def test_upload_from_one_local_text_file(self, *_):
+        self.kili.kili_api_gateway.get_project.return_value = {"inputType": "TEXT"}
         url = "https://storage.googleapis.com/label-public-staging/asset-test-sample/texts/test_text_file.txt"
         path = self.downloader(url)
         assets = [{"content": path, "external_id": "local text file"}]
@@ -34,6 +34,7 @@ class TextTestCase(ImportTestCase):
         self.kili.graphql_client.execute.assert_called_with(*expected_parameters)
 
     def test_upload_from_one_hosted_text_file(self, *_):
+        self.kili.kili_api_gateway.get_project.return_value = {"inputType": "TEXT"}
         assets = [
             {"content": "https://hosted-data", "external_id": "hosted file", "id": "unique_id"}
         ]
@@ -44,6 +45,7 @@ class TextTestCase(ImportTestCase):
         self.kili.graphql_client.execute.assert_called_with(*expected_parameters)
 
     def test_upload_from_raw_text(self, *_):
+        self.kili.kili_api_gateway.get_project.return_value = {"inputType": "TEXT"}
         assets = [{"content": "this is raw text", "external_id": "raw text"}]
         import_assets(self.kili, self.project_id, assets)
         expected_parameters = self.get_expected_sync_call(
@@ -52,6 +54,7 @@ class TextTestCase(ImportTestCase):
         self.kili.graphql_client.execute.assert_called_with(*expected_parameters)
 
     def test_upload_from_one_rich_text(self, *_):
+        self.kili.kili_api_gateway.get_project.return_value = {"inputType": "TEXT"}
         json_content = [
             {
                 "children": [
@@ -76,9 +79,11 @@ class TextTestCase(ImportTestCase):
         self.kili.graphql_client.execute.assert_called_with(*expected_parameters)
 
     def test_upload_from_several_batches(self, *_):
+        self.kili.kili_api_gateway.get_project.return_value = {"inputType": "TEXT"}
         self.assert_upload_several_batches()
 
     def test_upload_from_one_hosted_text_authorized_while_local_forbidden(self, *_):
+        self.kili.kili_api_gateway.get_project.return_value = {"inputType": "TEXT"}
         self.kili.kili_api_gateway.list_organizations = MagicMock(
             return_value=organization_generator(upload_local_data=False)
         )
