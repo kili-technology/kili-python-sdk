1c75a6b5a51d1101fc259ec73840a9536954fa1b
Sihem Tchabi <sihem@pixel.home>
2025-02-28 15:01:53 +0100

    fix(LAB-3518): fix export in coco and voc format for videos

--- src/kili/services/export/format/voc/__init__.py
+++ src/kili/services/export/format/voc/__init__.py
@@ -5,6 +5,8 @@ from pathlib import Path
 from typing import Dict, List, Sequence
 from xml.dom import minidom
 
+import requests
+
 from kili.domain.ontology import JobMLTask, JobTool
 from kili.services.export.exceptions import (
     NoCompatibleJobError,
@@ -91,8 +93,20 @@ def _process_asset(
             width, height = get_frame_dimensions(asset)
             frame_ext = Path(asset["jsonContent"][0]).suffix
 
+        elif (
+            isinstance(asset["jsonContent"], str)
+            and asset["jsonContent"] != ""
+            and requests.get(asset["jsonContent"], timeout=100).status_code == 200
+        ):
+            width, height = get_frame_dimensions(asset)
+            response = requests.get(asset["jsonContent"], timeout=100).json()
+            frame_ext = Path(response["0"]).suffix
+
         # video with shouldUseNativeVideo set to True (no frames available)
-        elif Path(asset["content"]).is_file():
+        elif Path(asset["content"]).is_file() or (
+            asset["content"] != ""
+            and requests.get(asset["content"], timeout=100).status_code == 200
+        ):
             width, height = get_video_dimensions(asset)
             cut_video(asset["content"], asset, leading_zeros, Path(asset["content"]).parent)
             frame_ext = ".jpg"
