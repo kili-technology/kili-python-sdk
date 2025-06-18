7da6caa8ee70fe79bb336123f07f7e5e7374d236
@fanny.gaudin <fanny.gaudin@kili-technology.com>
2025-06-18 15:13:21 +0200

    [main] fix(LAB-3755): add asset to patch_label_json_response for calculate width and height on VIDEO project

--- src/kili/adapters/kili_api_gateway/label/common.py
+++ src/kili/adapters/kili_api_gateway/label/common.py
@@ -1,8 +1,38 @@
 """Label gateway common."""
+from typing import Dict
+
+from kili.adapters.http_client import HttpClient
+from kili.adapters.kili_api_gateway.asset.formatters import load_asset_json_fields
+from kili.adapters.kili_api_gateway.asset.operations import get_assets_query
 from kili.adapters.kili_api_gateway.helpers.queries import fragment_builder
 from kili.adapters.kili_api_gateway.label.operations import (
     get_annotations_partial_query,
 )
+from kili.core.graphql.graphql_client import GraphQLClient
+from kili.domain.asset.asset import AssetId
+from kili.domain.types import ListOrTuple
+from kili.exceptions import NotFound
+
+
+def get_asset(
+    graphql_client: GraphQLClient,
+    http_client: HttpClient,
+    asset_id: AssetId,
+    fields: ListOrTuple[str],
+) -> Dict:
+    """Get asset."""
+    fragment = fragment_builder(fields)
+    query = get_assets_query(fragment)
+    result = graphql_client.execute(
+        query=query, variables={"where": {"id": asset_id}, "first": 1, "skip": 0}
+    )
+    assets = result["data"]
+
+    if len(assets) == 0:
+        raise NotFound(
+            f"asset ID: {asset_id}. The asset does not exist or you do not have access to it."
+        )
+    return load_asset_json_fields(assets[0], fields, http_client=http_client)
 
 
 def get_annotation_fragment():
